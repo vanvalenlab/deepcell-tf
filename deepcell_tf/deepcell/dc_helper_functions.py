477f95ffb2e92cdc6a7df813bc276ee498101839
William Graf <William.Graf@kp.org>
2018-05-16 12:05:46 -0700

    Removed unused imports.

--- deepcell_tf/deepcell/dc_helper_functions.py
+++ deepcell_tf/deepcell/dc_helper_functions.py
@@ -6,56 +6,23 @@ Functions for making training data
 @author: David Van Valen
 """
 
-"""
-Import python packages
-"""
-
-import numpy as np
-from numpy import array
-import matplotlib
-import matplotlib.pyplot as plt
-import shelve
-from contextlib import closing
+from __future__ import print_function
 
 import os
-import glob
 import re
-import numpy as np
-import fnmatch
-from . import tifffile as tiff
-from numpy.fft import fft2, ifft2, fftshift
-from skimage.io import imread
-from scipy import ndimage
-import threading
-import scipy.ndimage as ndi
-from scipy import linalg
-import re
-import random
-import itertools
-import h5py
-import datetime
 
-from skimage.measure import label, regionprops
-from skimage.segmentation import clear_border
-from scipy.ndimage.morphology import binary_fill_holes
-from skimage import morphology as morph
-from numpy.fft import fft2, ifft2, fftshift
-from skimage.io import imread
-from skimage.filters import threshold_otsu
-import skimage as sk
-from sklearn.utils.linear_assignment_ import linear_assignment
-from sklearn.utils import class_weight
+import numpy as np
+
+from scipy import ndimage
 from scipy.ndimage.filters import uniform_filter
 
+from skimage.io import imread
 
 import tensorflow as tf
-from tensorflow import keras
 from tensorflow.python.keras import backend as K
-from tensorflow.python.keras.layers import Layer, InputSpec, Input, Activation, Dense, Flatten, BatchNormalization, Conv2D, MaxPool2D, AvgPool2D, Concatenate
-from tensorflow.python.keras.preprocessing.image import random_rotation, random_shift, random_shear, random_zoom, random_channel_shift, apply_transform, flip_axis, array_to_img, img_to_array, load_img, ImageDataGenerator, Iterator, NumpyArrayIterator, DirectoryIterator
-from tensorflow.python.keras.callbacks import ModelCheckpoint, LearningRateScheduler
-from tensorflow.python.keras import activations, initializers, losses, regularizers, constraints
-from tensorflow.python.keras._impl.keras.utils import conv_utils
+from tensorflow.python.keras import activations
+
+from . import tifffile as tiff
 
 """
 Helper functions
