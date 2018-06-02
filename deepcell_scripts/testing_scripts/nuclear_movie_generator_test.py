987072260154d802b20f05c4747bf94e442b0b53
William Graf <7930703+willgraf@users.noreply.github.com>
2018-06-01 17:04:37 -0700

    getting ready for training

--- deepcell_scripts/testing_scripts/nuclear_movie_generator_test.py
+++ deepcell_scripts/testing_scripts/nuclear_movie_generator_test.py
@@ -1,16 +1,30 @@
+from tensorflow.python.keras.optimizers import SGD
+from tensorflow.python.keras import backend as K
+import numpy as np
 from deepcell import train_model_siamese, rate_scheduler
+from deepcell import bn_dense_feature_net_3D as the_model
 
 def main():
-    train_model_siamese(model=None,
+    direc_data = '/data/training_data/training_data_npz/nuclear_movie/'
+    dataset = 'nuclear_movie_same'
+    
+    training_data = np.load('{}{}.npz'.format(direc_data, dataset))
+
+    optimizer = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    lr_sched = rate_scheduler(lr=0.01, decay=0.99)
+    in_shape = (1, 60, 504, 487, 1)
+    model = the_model(batch_shape=in_shape, n_features=1, reg=1e-5, location=True)
+    
+    train_model_siamese(model=model,
                         dataset='nuclear_movie_same',
-                        optimizer=None,
+                        optimizer=optimizer,
                         expt='',
                         it=0,
                         batch_size=1,
                         n_epoch=100,
                         direc_save='/data/training_data/trained_networks/',
                         direc_data='/data/training_data/training_data_npz/nuclear_movie/',
-                        lr_sched=rate_scheduler(lr=0.01, decay=0.95),
+                        lr_sched=lr_sched,
                         rotation_range=0,
                         flip=True,
                         shear=0,
