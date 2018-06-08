86303edb8f7ba12a1859c69488c783e5e323dd4b
William Graf <7930703+willgraf@users.noreply.github.com>
2018-06-08 09:42:42 -0700

    exporting dilated_bn_feature_net_61x61 from dc_model_zoo

--- deepcell_scripts/testing_scripts/test_model_export.py
+++ deepcell_scripts/testing_scripts/test_model_export.py
@@ -17,11 +17,15 @@ import os
 import warnings
 
 from deepcell import export_model
-from deepcell.model_zoo import dilated_feature_net_61x61 as model_fn
+from deepcell.dc_model_zoo import dilated_bn_feature_net_61x61 as model_fn
 
 weights_path = '/models/2018-05-25_ecoli_kc_polaris_61x61__0.h5'
 export_path = '/exported_models'
 
+input_shape = (1280,1080,1)
+n_features = 3
+
+keras_model = model_fn(input_shape=input_shape, n_features=n_features)
 export_model(keras_model, export_path, weights_path = weights_path)
 
 
