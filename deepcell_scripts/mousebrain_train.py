0ac2f3d18e97c8ee730ebb6ef83f5dadd89edb4a
William Graf <7930703+willgraf@users.noreply.github.com>
2018-06-12 22:59:26 -0700

    Cleaned up mousebrain script

--- deepcell_scripts/mousebrain_train.py
+++ deepcell_scripts/mousebrain_train.py
@@ -1,29 +1,28 @@
 ## Generate training data
 import os
 import pdb
-import platform
 import argparse
 
+import numpy as np
+import tifffile as tiff
 from tensorflow.python.keras import backend as K
 from tensorflow.python.keras.optimizers import SGD, Adam
 
-import numpy as np
-
 from deepcell import make_training_data
-#from deepcell import bn_dense_multires_feature_net_3D as the_model
-#from deepcell import bn_dense_feature_net_lstm as the_model
 from deepcell import bn_dense_feature_net_3D as the_model
-#from deepcell import siamese_model as the_model
-from deepcell import rate_scheduler, train_model_movie as train_model
+from deepcell import bn_dense_multires_feature_net_3D
+from deepcell import bn_dense_feature_net_lstm
+from deepcell import siamese_model
+from deepcell import rate_scheduler
+from deepcell import train_model_movie as train_model
 from deepcell import load_training_images_3d
 from deepcell import run_model
-import tifffile as tiff
 
 # data options
 DATA_OUTPUT_MODE = 'conv'
 RESIZE = False
 RESHAPE_SIZE = 128
-NUM_FRAMES = 30
+NUM_FRAMES = 30 # get first N frames from each training folder
 
 # filepath constants
 DATA_DIR = '/data/data'
@@ -51,7 +50,7 @@ def generate_training_data():
         output_mode=DATA_OUTPUT_MODE,
         window_size_x=30,
         window_size_y=30,
-        border_mode='same',
+        border_mode='valid' if DATA_OUTPUT_MODE == 'sample' else 'same',
         reshape_size=None if not RESIZE else RESHAPE_SIZE,
         process=True,
         process_std=True,
@@ -68,41 +67,49 @@ def generate_training_data():
 def train_model_on_training_data():
     direc_save = os.path.join(MODEL_DIR, PREFIX)
     direc_data = os.path.join(NPZ_DIR, PREFIX)
-    batch_size = 1
-    n_epoch = 50
-    number_of_frames = 10
-
-    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-
     training_data = np.load(os.path.join(direc_data, DATA_FILE + '.npz'))
+
     X, y = training_data['X'], training_data['y']
     print('X.shape: {}\ny.shape: {}'.format(X.shape, y.shape))
 
-    if K.image_data_format() == 'channels_first':
-        row_size, col_size = X.shape[3:] if not RESIZE else (RESHAPE_SIZE, RESHAPE_SIZE)
-        batch_shape = (batch_size, X.shape[1], number_of_frames, row_size, col_size)
-    else:
-        row_size, col_size = X.shape[2:4] if not RESIZE else (RESHAPE_SIZE, RESHAPE_SIZE)
-        batch_shape = (batch_size, number_of_frames, row_size, col_size, X.shape[4])
+    n_epoch = 50
+    batch_size = 1
+    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    frames_per_batch = 10
 
-    n_features = 2 # np.unique(y).size
-    model = the_model(batch_shape=batch_shape, n_features=n_features,
-                      permute=False, location=False)
+    model_args = {
+        'n_features': 2, # np.unique(y).size
+        'permute': False,
+        'location': False
+    }
+
+    data_format = K.image_data_format()
+    row_axis = 3 if data_format == 'channels_first' else 2
+    col_axis = 4 if data_format == 'channels_first' else 3
+    channel_axis = 1 if data_format == 'channels_first' else 4
+
+    nrow, ncol = X.shape[row_axis:col_axis + 1] if not RESIZE else (RESHAPE_SIZE, RESHAPE_SIZE)
+    if K.image_data_format() == 'channels_first':
+        batch_shape = (batch_size, X.shape[channel_axis], frames_per_batch, nrow, ncol)
+    else:
+        batch_shape = (batch_size, frames_per_batch, nrow, ncol, X.shape[channel_axis])
+    model_args['batch_shape'] = batch_shape
+
+    model = the_model(**model_args)
 
     train_model(
         model=model,
         dataset=DATA_FILE,
         optimizer=sgd,
         batch_size=batch_size,
-        number_of_frames=number_of_frames,
+        number_of_frames=frames_per_batch,
         n_epoch=n_epoch,
         direc_save=direc_save,
         direc_data=direc_data,
         lr_sched=rate_scheduler(lr=0.01, decay=0.95),
         rotation_range=0,
         flip=False,
-        shear=0
-    )
+        shear=0)
 
 
 def run_model_on_dir():
