3dbdfe4e329e900d1f7456ab93aca9ab83f95fcf
Shivamshaiv <patelshivam99@gmail.com>
2018-06-28 00:13:25 +0530

    Modified the Model for 1D images

--- deepcell/model_zoo.py
+++ deepcell/model_zoo.py
@@ -1637,7 +1637,7 @@ def watershednetwork(pretrained_weights = None,input_size = (256,256,1)):
     conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
     conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
     conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-    conv10 = Conv2D(2, 1, activation = 'sigmoid')(conv9)
+    conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
 
     model = Model(inputs = inputs, outputs = conv10)
     return model
