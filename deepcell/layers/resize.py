a027e9da0c9d2cfb4888fadb12eabac400b642af
William Graf <7930703+willgraf@users.noreply.github.com>
2018-07-17 14:21:24 -0700

    added tf version comment with explanation

--- deepcell/layers/resize.py
+++ deepcell/layers/resize.py
@@ -12,7 +12,7 @@ from __future__ import division
 import tensorflow as tf
 from tensorflow.python.keras import backend as K
 from tensorflow.python.keras.layers import Layer
-try:
+try:  # tf v1.9 moves conv_utils from _impl to keras.utils
     from tensorflow.python.keras.utils import conv_utils
 except ImportError:
     from tensorflow.python.keras._impl.keras.utils import conv_utils
--- deepcell/layers/tensor_product.py
+++ deepcell/layers/tensor_product.py
@@ -17,7 +17,7 @@ from tensorflow.python.keras import initializers
 from tensorflow.python.keras import regularizers
 from tensorflow.python.keras.layers import Layer
 from tensorflow.python.keras.layers import InputSpec
-try:
+try:  # tf v1.9 moves conv_utils from _impl to keras.utils
     from tensorflow.python.keras.utils import conv_utils
 except ImportError:
     from tensorflow.python.keras._impl.keras.utils import conv_utils
