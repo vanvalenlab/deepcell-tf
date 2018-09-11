a6e8068e2179c3d567e2defcb435580561426ba8
William Graf <7930703+willgraf@users.noreply.github.com>
2018-09-11 15:51:54 -0700

    export_model keeps weights and works with skip_connections

--- deepcell/utils/export_utils.py
+++ deepcell/utils/export_utils.py
@@ -15,13 +15,10 @@ import tensorflow as tf
 from tensorflow.python.keras import backend as K
 from tensorflow.python.saved_model import tag_constants
 from tensorflow.python.saved_model import signature_constants
-from tensorflow.python.saved_model import signature_def_utils
-from tensorflow.python.saved_model import signature_def_utils_impl
 from tensorflow.python.saved_model.builder import SavedModelBuilder
 
 
 def export_model(keras_model, export_path, model_version=0, weights_path=None):
-
     # Start the tensorflow session
     gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.8, allow_growth=False)
     sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
@@ -30,24 +27,29 @@ def export_model(keras_model, export_path, model_version=0, weights_path=None):
     K._LEARNING_PHASE = tf.constant(0)
     K.set_learning_phase(0)
 
-    # Load the model and the weights
-    if weights_path is not None:
-        keras_model.load_weights(weights_path)
-
-    # Define prediction signature
-    prediction_signature = tf.saved_model.signature_def_utils.predict_signature_def(
-        {'image': keras_model.input},
-        {'prediction': keras_model.output}
-    )
-
     # Create export path if it doesn't exist
     export_path = os.path.join(export_path, str(model_version))
     builder = SavedModelBuilder(export_path)
-    legacy_init_op = tf.group(tf.tables_initializer(), name='legacy_init_op')
+    # legacy_init_op = tf.group(tf.tables_initializer(), name='legacy_init_op')
 
     # Initialize global variables and the model
     init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
     sess.run(init_op)
+
+    # Load the model and the weights
+    if weights_path is not None:
+        keras_model.load_weights(weights_path)
+
+    if isinstance(keras_model.output, list):
+        output = keras_model.output[-1]
+    else:
+        output = keras_model.output
+
+    # Define prediction signature
+    prediction_signature = tf.saved_model.signature_def_utils.predict_signature_def(
+        {'image': keras_model.input},
+        {'prediction': output}
+    )
 
     # Add the meta_graph and the variables to the builder
     builder.add_meta_graph_and_variables(
@@ -55,8 +57,7 @@ def export_model(keras_model, export_path, model_version=0, weights_path=None):
         signature_def_map={
             signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY:
                 prediction_signature
-        },
-        legacy_init_op=legacy_init_op)
+        })
 
     # Save the graph
     builder.save()
