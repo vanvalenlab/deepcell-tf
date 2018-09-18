ff0fde8fff93e40c1a3047e3b535e47688e0db6a
William Graf <7930703+willgraf@users.noreply.github.com>
2018-09-18 16:20:50 -0700

    modified Apache 2.0 License

--- tests/deepcell/image_generators_test.py
+++ tests/deepcell/image_generators_test.py
@@ -1,8 +1,29 @@
-"""
-image_generators_test.py
-
-Tests for custom image data generators
-
+# Copyright 2016-2018 David Van Valen at California Institute of Technology
+# (Caltech), with support from the Paul Allen Family Foundation, Google,
+# & National Institutes of Health (NIH) under Grant U24CA224309-01.
+# All rights reserved.
+#
+# Licensed under a modified Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.github.com/vanvalenlab/deepcell-tf/LICENSE
+#
+# The Work provided may be used for non-commercial academic purposes only.
+# For any other use of the Work, including commercial use, please contact:
+# vanvalenlab@gmail.com
+#
+# Neither the name of Caltech nor the names of its contributors may be used
+# to endorse or promote products derived from this software without specific
+# prior written permission.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Tests for custom image data generators
 @author: David Van Valen
 """
 
