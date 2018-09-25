2371cd596cdc7b1b08b237f638f94f8e46c9b8d7
William Graf <7930703+willgraf@users.noreply.github.com>
2018-09-25 12:56:40 -0700

    added scripts to automatically generate jupyter notebooks for predict and train

--- deepcell/notebooks/__init__.py
+++ deepcell/notebooks/__init__.py
@@ -23,25 +23,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Package for single cell image segmentation with convolutional neural networks
-@author: David Van Valen
-"""
+"""Automatically generate jupyter notebooks"""
 from __future__ import absolute_import
 
-from . import datasets
-from . import layers
-from . import losses
-from . import image_generators
-from . import model_zoo
-from . import notebooks
-from . import running
-from . import training
-from . import utils
-
-from .layers import *
-from .losses import *
-from .image_generators import *
-from .model_zoo import *
-from .running import *
-from .training import *
-from .utils import *
+from deepcell.notebooks import predict
+from deepcell.notebooks import train
---
+++ deepcell/notebooks/predict.py
@@ -0,0 +1,102 @@
+# Copyright 2016-2018 David Van Valen at California Institute of Technology
+# (Caltech), with support from the Paul Allen Family Foundation, Google,
+# & National Institutes of Health (NIH) under Grant U24CA224309-01.
+# All rights reserved.
+#
+# Licensed under a modified Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.github.com/vanvalenlab/deepcell-tf/LICENSE
+#
+# The Work provided may be used for non-commercial academic purposes only.
+# For any other use of the Work, including commercial use, please contact:
+# vanvalenlab@gmail.com
+#
+# Neither the name of Caltech nor the names of its contributors may be used
+# to endorse or promote products derived from this software without specific
+# prior written permission.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Automatically generate a notebook for analyzing predicted images
+@author: andrewqho, willgraf
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import hashlib
+import os
+import shutil
+import time
+
+import nbformat as nbf
+
+
+# Create Visual Notebook
+def make_notebook(model_output):
+    """Create a visualization notebook that will help visualize
+    the output of a deep learning model
+    # Arguments:
+        model_output: output of a deep learning model to visualize
+    """
+    # list of cells that will be in the notebook
+    cells = []
+
+    # Markdown Header
+    text = [
+        '## Visualize Predictions'
+        'A template Jupyter notebook to explore returned predictions'
+    ]
+    cells.append(nbf.v4.new_markdown_cell('\n'.join(text)))
+
+    # Python imports
+    imports = [
+        'from matplotlib import pyplot as plt',
+        'import numpy as np',
+        'from PIL import Image'
+    ]
+    cells.append(nbf.v4.new_code_cell('\n'.join(imports)))
+
+    # Code for Data Visualization
+    visual = [
+        'pil_im = Image.open("{}", "r")'.format(model_output),
+        'plt.imshow(np.asarray(pil_im))'
+    ]
+    cells.append((nbf.v4.new_code_cell('\n'.join(visual))))
+
+    # Create and write to new ipynb
+    nb = nbf.v4.new_notebook(cells=cells)
+    nbf.write(nb, 'visualize.ipynb')
+
+    # Move output image file to "notebook" directory
+    # path = os.path.join('notebooks', os.path.basename(model_output))
+    # shutil.move(model_output, path)
+
+    # # Change CWD to "notebook" directory
+    # os.chdir('notebooks')
+
+    # # Create directory for notebook and data
+    # ts = '{}'.format(time.time()).encode('utf-8')
+    # hashed_directory = 'visual_{}'.format(hashlib.md5(ts).hexdigest())
+    # os.mkdir(hashed_directory)
+
+    # # Move output image file to new directory
+    # path = os.path.join(hashed_directory, os.path.basename(model_output))
+    # shutil.move(model_output, path)
+
+    # # Change CWD to specific directory
+    # os.chdir(os.path.join('.', hashed_directory))
+
+    # # Create data directory
+    # os.mkdir('data')
+
+    # # Move output image file to data directory
+    # path = os.path.join('data', os.path.basename(model_output))
+    # shutil.move(model_output, path)
