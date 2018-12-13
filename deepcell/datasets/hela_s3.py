7ffa1f7603716dcdd2bd9a5518e6a49b3240e6b8
William Graf <7930703+willgraf@users.noreply.github.com>
2018-12-12 22:04:13 -0800

    remove python from tensorflow.keras import

--- deepcell/datasets/hela_s3.py
+++ deepcell/datasets/hela_s3.py
@@ -29,9 +29,9 @@ from __future__ import division
 from __future__ import print_function
 
 try:
-    from tensorflow.python.keras.utils.data_utils import get_file
+    from tensorflow.keras.utils.data_utils import get_file
 except ImportError:  # tf v1.9 moves conv_utils from _impl to keras.utils
-    from tensorflow.python.keras._impl.keras.utils.data_utils import get_file
+    from tensorflow.keras._impl.keras.utils.data_utils import get_file
 
 from deepcell.utils.data_utils import get_data
 
