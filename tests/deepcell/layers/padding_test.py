6be58e42530f22082e47440cef7b45be5d99c6a9
William Graf <7930703+willgraf@users.noreply.github.com>
2018-12-15 19:41:21 -0800

    commented out correctness test

--- tests/deepcell/layers/padding_test.py
+++ tests/deepcell/layers/padding_test.py
@@ -31,11 +31,12 @@ from __future__ import print_function
 
 import numpy as np
 
+from tensorflow.python import keras
+from tensorflow.python.eager import context
 from tensorflow.python.framework import test_util as tf_test_util
-from tensorflow.python.keras import testing_utils
-from tensorflow.python.ops import array_ops
 from tensorflow.python.platform import test
 
+from deepcell.utils import testing_utils
 from deepcell import layers
 
 
@@ -46,241 +47,131 @@ def _get_random_padding(dim):
 
 class ReflectionPaddingTest(test.TestCase):
 
-    # @tf_test_util.run_in_graph_and_eager_modes()
-    # def test_reflection_padding_2d(self):
-    #     num_samples = 2
-    #     stack_size = 2
-    #     input_num_row = 4
-    #     input_num_col = 5
-    #     for data_format in ['channels_first', 'channels_last']:
-    #         inputs = np.ones((num_samples, input_num_row, input_num_col, stack_size))
-    #         inputs = np.ones((num_samples, stack_size, input_num_row, input_num_col))
+    @tf_test_util.run_in_graph_and_eager_modes()
+    def test_reflection_padding_2d(self):
+        num_samples = 2
+        stack_size = 2
+        input_num_row = 4
+        input_num_col = 5
 
-    #         # basic test
-    #         with self.test_session(use_gpu=True):
-    #             testing_utils.layer_test(
-    #                 layers.ReflectionPadding2D,
-    #                 kwargs={'padding': (2, 2),
-    #                         'data_format': data_format},
-    #                 input_shape=inputs.shape)
-    #             testing_utils.layer_test(
-    #                 layers.ReflectionPadding2D,
-    #                 kwargs={'padding': ((1, 2), (3, 4)),
-    #                         'data_format': data_format},
-    #                 input_shape=inputs.shape)
+        inputs1 = np.ones((num_samples, input_num_row, input_num_col, stack_size))
+        inputs2 = np.ones((num_samples, stack_size, input_num_row, input_num_col))
+        data_formats = ['channels_first', 'channels_last']
+        for data_format, inputs in zip(data_formats, [inputs2, inputs1]):
+            # basic test
+            with self.test_session(use_gpu=True):
+                testing_utils.layer_test(
+                    layers.ReflectionPadding2D,
+                    kwargs={'padding': (2, 2),
+                            'data_format': data_format},
+                    custom_objects={'ReflectionPadding2D': layers.ReflectionPadding2D},
+                    input_shape=inputs.shape)
+                testing_utils.layer_test(
+                    layers.ReflectionPadding2D,
+                    kwargs={'padding': ((1, 2), (3, 4)),
+                            'data_format': data_format},
+                    custom_objects={'ReflectionPadding2D': layers.ReflectionPadding2D},
+                    input_shape=inputs.shape)
 
-    #         # correctness test
-    #         with self.test_session(use_gpu=True):
-    #             layer = layers.ReflectionPadding2D(
-    #                 padding=(2, 2), data_format=data_format)
-    #             layer.build(inputs.shape)
-    #             output = layer(keras.backend.variable(inputs))
-    #             if context.executing_eagerly():
-    #                 np_output = output.numpy()
-    #             else:
-    #                 np_output = keras.backend.eval(output)
-    #             if data_format == 'channels_last':
-    #                 for offset in [0, 1, -1, -2]:
-    #                     np.testing.assert_allclose(np_output[:, offset, :, :], 0.)
-    #                     np.testing.assert_allclose(np_output[:, :, offset, :], 0.)
-    #                 np.testing.assert_allclose(np_output[:, 2:-2, 2:-2, :], 1.)
-    #             elif data_format == 'channels_first':
-    #                 for offset in [0, 1, -1, -2]:
-    #                     np.testing.assert_allclose(np_output[:, :, offset, :], 0.)
-    #                     np.testing.assert_allclose(np_output[:, :, :, offset], 0.)
-    #                 np.testing.assert_allclose(np_output[:, 2:-2, 2:-2, :], 1.)
+            # correctness test
+            # with self.test_session(use_gpu=True):
+            #     layer = layers.ReflectionPadding2D(
+            #         padding=(2, 2), data_format=data_format)
+            #     layer.build(inputs.shape)
+            #     output = layer(keras.backend.variable(inputs))
+            #     if context.executing_eagerly():
+            #         np_output = output.numpy()
+            #     else:
+            #         np_output = keras.backend.eval(output)
+            #     if data_format == 'channels_last':
+            #         for offset in [0, 1, -1, -2]:
+            #             np.testing.assert_allclose(np_output[:, offset, :, :], 0.)
+            #             np.testing.assert_allclose(np_output[:, :, offset, :], 0.)
+            #         np.testing.assert_allclose(np_output[:, 2:-2, 2:-2, :], 1.)
+            #     elif data_format == 'channels_first':
+            #         for offset in [0, 1, -1, -2]:
+            #             np.testing.assert_allclose(np_output[:, :, offset, :], 0.)
+            #             np.testing.assert_allclose(np_output[:, :, :, offset], 0.)
+            #         np.testing.assert_allclose(np_output[:, 2:-2, 2:-2, :], 1.)
 
-    #             layer = layers.ReflectionPadding2D(
-    #                 padding=((1, 2), (3, 4)), data_format=data_format)
-    #             layer.build(inputs.shape)
-    #             output = layer(keras.backend.variable(inputs))
-    #             if context.executing_eagerly():
-    #                 np_output = output.numpy()
-    #             else:
-    #                 np_output = keras.backend.eval(output)
-    #             if data_format == 'channels_last':
-    #                 for top_offset in [0]:
-    #                     np.testing.assert_allclose(np_output[:, top_offset, :, :], 0.)
-    #                 for bottom_offset in [-1, -2]:
-    #                     np.testing.assert_allclose(np_output[:, bottom_offset, :, :], 0.)
-    #                 for left_offset in [0, 1, 2]:
-    #                     np.testing.assert_allclose(np_output[:, :, left_offset, :], 0.)
-    #                 for right_offset in [-1, -2, -3, -4]:
-    #                     np.testing.assert_allclose(np_output[:, :, right_offset, :], 0.)
-    #                 np.testing.assert_allclose(np_output[:, 1:-2, 3:-4, :], 1.)
-    #             elif data_format == 'channels_first':
-    #                 for top_offset in [0]:
-    #                     np.testing.assert_allclose(np_output[:, :, top_offset, :], 0.)
-    #                 for bottom_offset in [-1, -2]:
-    #                     np.testing.assert_allclose(np_output[:, :, bottom_offset, :], 0.)
-    #                 for left_offset in [0, 1, 2]:
-    #                     np.testing.assert_allclose(np_output[:, :, :, left_offset], 0.)
-    #                 for right_offset in [-1, -2, -3, -4]:
-    #                     np.testing.assert_allclose(np_output[:, :, :, right_offset], 0.)
-    #                 np.testing.assert_allclose(np_output[:, :, 1:-2, 3:-4], 1.)
+            #     layer = layers.ReflectionPadding2D(
+            #         padding=((1, 2), (3, 4)), data_format=data_format)
+            #     layer.build(inputs.shape)
+            #     output = layer(keras.backend.variable(inputs))
+            #     if context.executing_eagerly():
+            #         np_output = output.numpy()
+            #     else:
+            #         np_output = keras.backend.eval(output)
+            #     if data_format == 'channels_last':
+            #         for top_offset in [0]:
+            #             np.testing.assert_allclose(np_output[:, top_offset, :, :], 0.)
+            #         for bottom_offset in [-1, -2]:
+            #             np.testing.assert_allclose(np_output[:, bottom_offset, :, :], 0.)
+            #         for left_offset in [0, 1, 2]:
+            #             np.testing.assert_allclose(np_output[:, :, left_offset, :], 0.)
+            #         for right_offset in [-1, -2, -3, -4]:
+            #             np.testing.assert_allclose(np_output[:, :, right_offset, :], 0.)
+            #         np.testing.assert_allclose(np_output[:, 1:-2, 3:-4, :], 1.)
+            #     elif data_format == 'channels_first':
+            #         for top_offset in [0]:
+            #             np.testing.assert_allclose(np_output[:, :, top_offset, :], 0.)
+            #         for bottom_offset in [-1, -2]:
+            #             np.testing.assert_allclose(np_output[:, :, bottom_offset, :], 0.)
+            #         for left_offset in [0, 1, 2]:
+            #             np.testing.assert_allclose(np_output[:, :, :, left_offset], 0.)
+            #         for right_offset in [-1, -2, -3, -4]:
+            #             np.testing.assert_allclose(np_output[:, :, :, right_offset], 0.)
+            #         np.testing.assert_allclose(np_output[:, :, 1:-2, 3:-4], 1.)
 
-    #         # test incorrect use
-    #         with self.assertRaises(ValueError):
-    #             layers.ReflectionPadding2D(padding=(1, 1, 1))
-    #         with self.assertRaises(ValueError):
-    #             layers.ReflectionPadding2D(padding=None)
-
-    # @tf_test_util.run_in_graph_and_eager_modes()
-    # def test_reflection_padding_3d(self):
-    #     num_samples = 2
-    #     stack_size = 2
-    #     input_len_dim1 = 4
-    #     input_len_dim2 = 5
-    #     input_len_dim3 = 3
-
-    #     inputs = np.ones((num_samples, input_len_dim1, input_len_dim2,
-    #                       input_len_dim3, stack_size))
-
-    #     # basic test
-    #     with self.test_session(use_gpu=True):
-    #         testing_utils.layer_test(
-    #             layers.ReflectionPadding3D,
-    #             kwargs={'padding': (2, 2, 2)},
-    #             input_shape=inputs.shape)
-
-    #     # correctness test
-    #     with self.test_session(use_gpu=True):
-    #         layer = layers.ReflectionPadding3D(padding=(2, 2, 2))
-    #         layer.build(inputs.shape)
-    #         output = layer(keras.backend.variable(inputs))
-    #         if context.executing_eagerly():
-    #             np_output = output.numpy()
-    #         else:
-    #             np_output = keras.backend.eval(output)
-    #         for offset in [0, 1, -1, -2]:
-    #             np.testing.assert_allclose(np_output[:, offset, :, :, :], 0.)
-    #             np.testing.assert_allclose(np_output[:, :, offset, :, :], 0.)
-    #             np.testing.assert_allclose(np_output[:, :, :, offset, :], 0.)
-    #         np.testing.assert_allclose(np_output[:, 2:-2, 2:-2, 2:-2, :], 1.)
-
-    #     # test incorrect use
-    #     with self.assertRaises(ValueError):
-    #         layers.ReflectionPadding3D(padding=(1, 1))
-    #     with self.assertRaises(ValueError):
-    #         layers.ReflectionPadding3D(padding=None)
-
-    def test_reflect_padding_2d(self):
-        with self.test_session():
-            x, y, c = 4, 4, 1
-            i1 = array_ops.placeholder(shape=(None, x, y, c), dtype='float32')
-            pad = _get_random_padding(2)
-            layer = layers.ReflectionPadding2D(padding=pad)
-            o = layer(i1)
-            expected = [None, x + pad[0][0] + pad[0][1],
-                        y + pad[1][0] + pad[1][1], c]
-            self.assertListEqual(o.get_shape().as_list(), expected)
-
-    def test_reflect_padding_2d_channels_first(self):
-        with self.test_session():
-            x, y, c = 4, 4, 1
-            i1 = array_ops.placeholder(shape=(None, c, x, y), dtype='float32')
-            pad = _get_random_padding(2)
-            layer = layers.ReflectionPadding2D(
-                padding=pad, data_format='channels_first')
-            o = layer(i1)
-            expected = [None, c, x + pad[0][0] + pad[0][1],
-                        y + pad[1][0] + pad[1][1]]
-            self.assertListEqual(o.get_shape().as_list(), expected)
-
-    def test_reflect_padding_2d_tuple_input(self):
-        with self.test_session():
-            x, y, c = 4, 4, 1
-            i1 = array_ops.placeholder(shape=(None, x, y, c), dtype='float32')
-            pad = _get_random_padding(1)[0]
-            layer = layers.ReflectionPadding2D(padding=pad)
-            o = layer(i1)
-            expected = [None, x + 2 * pad[0], y + 2 * pad[1], c]
-            self.assertListEqual(o.get_shape().as_list(), expected)
-
-    def test_reflect_padding_2d_int_input(self):
-        with self.test_session():
-            x, y, c = 4, 4, 1
-            i1 = array_ops.placeholder(shape=(None, x, y, c), dtype='float32')
-            pad = np.random.randint(low=0, high=9)
-            layer = layers.ReflectionPadding2D(padding=pad)
-            o = layer(i1)
-            expected = [None, x + 2 * pad, y + 2 * pad, c]
-            self.assertListEqual(o.get_shape().as_list(), expected)
-
-    def test_reflect_padding_2d_bad_input_size(self):
-        with self.test_session():
-            pad = np.random.randint(low=0, high=9)
+            # test incorrect use
             with self.assertRaises(ValueError):
-                layers.ReflectionPadding2D(padding=(pad,))
-
-    def test_reflect_padding_2d_bad_input_type(self):
-        with self.test_session():
+                layers.ReflectionPadding2D(padding=(1, 1, 1))
             with self.assertRaises(ValueError):
                 layers.ReflectionPadding2D(padding=None)
 
-class ReflectionPadding3DTest(test.TestCase):
+    @tf_test_util.run_in_graph_and_eager_modes()
+    def test_reflection_padding_3d(self):
+        num_samples = 2
+        stack_size = 2
+        input_len_dim1 = 4
+        input_len_dim2 = 5
+        input_len_dim3 = 3
 
-    def test_reflect_padding_3d(self):
-        with self.test_session():
-            z, x, y, c = 3, 4, 4, 1
-            input_shape = (None, z, x, y, c)
-            i1 = array_ops.placeholder(shape=input_shape, dtype='float32')
-            pad = _get_random_padding(3)
-            layer = layers.ReflectionPadding3D(padding=pad)
-            o = layer(i1)
-            expected = [None, z + pad[0][0] + pad[0][1],
-                        x + pad[1][0] + pad[1][1],
-                        y + pad[2][0] + pad[2][1], c]
-            self.assertListEqual(o.get_shape().as_list(), expected)
+        inputs1 = np.ones((num_samples, input_len_dim1, input_len_dim2,
+                           input_len_dim3, stack_size))
+        inputs2 = np.ones((num_samples, stack_size, input_len_dim1,
+                           input_len_dim2, input_len_dim3))
+        data_formats = ['channels_first', 'channels_last']
+        for data_format, inputs in zip(data_formats, [inputs2, inputs1]):
+            # basic test
+            with self.test_session(use_gpu=True):
+                testing_utils.layer_test(
+                    layers.ReflectionPadding3D,
+                    kwargs={'padding': (2, 2, 2),
+                            'data_format': data_format},
+                    custom_objects={'ReflectionPadding3D': layers.ReflectionPadding3D},
+                    input_shape=inputs.shape)
 
-    def test_reflect_padding_3d_channels_first(self):
-        with self.test_session():
-            z, x, y, c = 3, 4, 4, 1
-            input_shape = (None, c, z, x, y)
-            i1 = array_ops.placeholder(shape=input_shape, dtype='float32')
-            pad = _get_random_padding(3)
-            layer = layers.ReflectionPadding3D(
-                padding=pad, data_format='channels_first')
-            o = layer(i1)
-            expected = [None, c, z + pad[0][0] + pad[0][1],
-                        x + pad[1][0] + pad[1][1],
-                        y + pad[2][0] + pad[2][1]]
-            self.assertListEqual(o.get_shape().as_list(), expected)
+        # correctness test
+        # with self.test_session(use_gpu=True):
+        #     layer = layers.ReflectionPadding3D(padding=(2, 2, 2))
+        #     layer.build(inputs.shape)
+        #     output = layer(keras.backend.variable(inputs))
+        #     if context.executing_eagerly():
+        #         np_output = output.numpy()
+        #     else:
+        #         np_output = keras.backend.eval(output)
+        #     for offset in [0, 1, -1, -2]:
+        #         np.testing.assert_allclose(np_output[:, offset, :, :, :], 0.)
+        #         np.testing.assert_allclose(np_output[:, :, offset, :, :], 0.)
+        #         np.testing.assert_allclose(np_output[:, :, :, offset, :], 0.)
+        #     np.testing.assert_allclose(np_output[:, 2:-2, 2:-2, 2:-2, :], 1.)
 
-    def test_reflect_z_padding_3d_tuple_input(self):
-        with self.test_session():
-            z, x, y, c = 3, 4, 4, 1
-            input_shape = (None, z, x, y, c)
-            i1 = array_ops.placeholder(shape=input_shape, dtype='float32')
-            padz = np.random.randint(low=0, high=9)
-            padx = np.random.randint(low=0, high=9)
-            pady = np.random.randint(low=0, high=9)
-            layer = layers.ReflectionPadding3D(padding=(padz, padx, pady))
-            o = layer(i1)
-            expected = [None, z + 2 * padz, x + 2 * padx, y + 2 * pady, c]
-            self.assertListEqual(o.get_shape().as_list(), expected)
-
-    def test_reflect_z_padding_3d_int_input(self):
-        with self.test_session():
-            z, x, y, c = 3, 4, 4, 1
-            input_shape = (None, z, x, y, c)
-            i1 = array_ops.placeholder(shape=input_shape, dtype='float32')
-            pad = np.random.randint(low=0, high=9)
-            layer = layers.ReflectionPadding3D(padding=pad)
-            o = layer(i1)
-            expected = [None, z + 2 * pad, x + 2 * pad, y + 2 * pad, c]
-            self.assertListEqual(o.get_shape().as_list(), expected)
-
-    def test_reflect_padding_3d_bad_input_size(self):
-        with self.test_session():
-            pad = np.random.randint(low=0, high=9)
-            with self.assertRaises(ValueError):
-                layers.ReflectionPadding3D(padding=(pad,))
-
-    def test_reflect_padding_3d_bad_input_type(self):
-        with self.test_session():
-            with self.assertRaises(ValueError):
-                layers.ReflectionPadding3D(padding=None)
+        # test incorrect use
+        with self.assertRaises(ValueError):
+            layers.ReflectionPadding3D(padding=(1, 1))
+        with self.assertRaises(ValueError):
+            layers.ReflectionPadding3D(padding=None)
 
 if __name__ == '__main__':
     test.main()
