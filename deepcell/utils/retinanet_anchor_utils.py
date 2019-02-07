8ad75d861cf07bddad7818d4080d94fa32066a09
William Graf <7930703+willgraf@users.noreply.github.com>
2019-02-07 14:38:14 -0800

    add evaluate to retinanet_utils

--- deepcell/utils/retinanet_anchor_utils.py
+++ deepcell/utils/retinanet_anchor_utils.py
@@ -465,3 +465,207 @@ def shift(shape, stride, anchors):
     shifted_anchors = K.reshape(shifted_anchors, [k * number_of_anchors, 4])
 
     return shifted_anchors
+
+
+
+def _compute_ap(recall, precision):
+    """Compute the average precision, given the recall and precision curves.
+
+    Code originally from https://github.com/rbgirshick/py-faster-rcnn.
+
+    # Arguments
+        recall: The recall curve (list).
+        precision: The precision curve (list).
+    # Returns
+        The average precision as computed in py-faster-rcnn.
+    """
+    # correct AP calculation
+    # first append sentinel values at the end
+    mrec = np.concatenate(([0.], recall, [1.]))
+    mpre = np.concatenate(([0.], precision, [0.]))
+
+    # compute the precision envelope
+    for i in range(mpre.size - 1, 0, -1):
+        mpre[i - 1] = np.maximum(mpre[i - 1], mpre[i])
+
+    # to calculate area under PR curve, look for points
+    # where X axis (recall) changes value
+    i = np.where(mrec[1:] != mrec[:-1])[0]
+
+    # and sum (\Delta recall) * prec
+    ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
+    return ap
+
+
+def _get_detections(generator,
+                    model,
+                    score_threshold=0.05,
+                    max_detections=100):
+    """Get the detections from the model using the generator.
+
+    The result is a list of lists such that the size is:
+        all_detections[num_images][num_classes] = detections[num_detections, 4 + num_classes]
+
+    # Arguments
+        generator: The generator used to run images through the model.
+        model: The model to run on the images.
+        score_threshold: The score confidence threshold to use.
+        max_detections: The maximum number of detections to use per image.
+    # Returns
+        A list of lists containing the detections for each image in the generator.
+    """
+    all_detections = [[None for i in range(generator.num_classes)]
+                      for j in range(generator.y.shape[0])]
+
+    for i in range(generator.y.shape[0]):
+        # raw_image = generator.load_image(i)
+        # image = generator.preprocess_image(raw_image.copy())
+        # image, scale = generator.resize_image(image)
+        image = generator.y[i]
+
+        # run network
+        results = model.predict_on_batch(np.expand_dims(image, axis=0))
+        boxes, scores, labels = results[:3]
+
+        # correct boxes for image scale
+        # boxes = boxes / scale
+
+        # select indices which have a score above the threshold
+        indices = np.where(scores[0, :] > score_threshold)[0]
+
+        # select those scores
+        scores = scores[0][indices]
+
+        # find the order with which to sort the scores
+        scores_sort = np.argsort(-scores)[:max_detections]
+
+        # select detections
+        image_boxes = boxes[0, indices[scores_sort], :]
+        image_scores = scores[scores_sort]
+        image_labels = labels[0, indices[scores_sort]]
+        image_detections = np.concatenate([
+            image_boxes,
+            np.expand_dims(image_scores, axis=1),
+            np.expand_dims(image_labels, axis=1)], axis=1)
+
+        # copy detections to all_detections
+        for label in range(generator.num_classes()):
+            all_detections[i][label] = image_detections[image_detections[:, -1] == label, :-1]
+
+    return all_detections
+
+
+def _get_annotations(generator):
+    """Get the ground truth annotations from the generator.
+
+    The result is a list of lists such that the size is:
+        all_detections[num_images][num_classes] = annotations[num_detections, 5]
+
+    # Arguments
+        generator : The generator used to retrieve ground truth annotations.
+    # Returns
+        A list of lists containing the annotations for each image in the generator.
+    """
+    all_annotations = [[None for i in range(generator.num_classes)]
+                       for j in range(generator.y.shape[0])]
+
+    for i in range(generator.size()):
+        # load the annotations
+        annotations = generator.load_annotations(i)
+
+        # copy detections to all_annotations
+        for label in range(generator.num_classes()):
+            il = annotations['bboxes'][annotations['labels'] == label, :].copy()
+            all_annotations[i][label] = il
+
+    return all_annotations
+
+
+def evaluate(generator,
+             model,
+             iou_threshold=0.5,
+             score_threshold=0.05,
+             max_detections=100):
+    """Evaluate a given dataset using a given model.
+
+    # Arguments
+        generator: The generator that represents the dataset to evaluate.
+        model: The model to evaluate.
+        iou_threshold: The threshold used to consider when a detection
+            is positive or negative.
+        score_threshold: The score confidence threshold to use for detections.
+        max_detections: The maximum number of detections to use per image.
+    # Returns
+        A dict mapping class names to mAP scores.
+    """
+    # gather all detections and annotations
+    all_detections = _get_detections(
+        generator, model,
+        score_threshold=score_threshold,
+        max_detections=max_detections)
+
+    all_annotations = _get_annotations(generator)
+    average_precisions = {}
+
+    # all_detections = pickle.load(open('all_detections.pkl', 'rb'))
+    # all_annotations = pickle.load(open('all_annotations.pkl', 'rb'))
+    # pickle.dump(all_detections, open('all_detections.pkl', 'wb'))
+    # pickle.dump(all_annotations, open('all_annotations.pkl', 'wb'))
+
+    # process detections and annotations
+    for label in range(generator.num_classes):
+        false_positives = np.zeros((0,))
+        true_positives = np.zeros((0,))
+        scores = np.zeros((0,))
+        num_annotations = 0.0
+
+        for i in range(generator.y.shape[0]):
+            detections = all_detections[i][label]
+            annotations = all_annotations[i][label]
+            num_annotations += annotations.shape[0]
+            detected = []
+
+            for d in detections:
+                scores = np.append(scores, d[4])
+
+                if annotations.shape[0] == 0:
+                    false_positives = np.append(false_positives, 1)
+                    true_positives = np.append(true_positives, 0)
+                    continue
+
+                overlaps = compute_overlap(np.expand_dims(d, axis=0), annotations)
+                assigned = np.argmax(overlaps, axis=1)
+                max_overlap = overlaps[0, assigned]
+
+                if max_overlap >= iou_threshold and assigned not in detected:
+                    false_positives = np.append(false_positives, 0)
+                    true_positives = np.append(true_positives, 1)
+                    detected.append(assigned)
+                else:
+                    false_positives = np.append(false_positives, 1)
+                    true_positives = np.append(true_positives, 0)
+
+        # no annotations -> AP for this class is 0 (is this correct?)
+        if num_annotations == 0:
+            average_precisions[label] = 0, 0
+            continue
+
+        # sort by score
+        indices = np.argsort(-scores)
+        false_positives = false_positives[indices]
+        true_positives = true_positives[indices]
+
+        # compute false positives and true positives
+        false_positives = np.cumsum(false_positives)
+        true_positives = np.cumsum(true_positives)
+
+        # compute recall and precision
+        recall = true_positives / num_annotations
+        precision = true_positives / np.maximum(true_positives + false_positives,
+                                                np.finfo(np.float64).eps)
+
+        # compute average precision
+        average_precision = _compute_ap(recall, precision)
+        average_precisions[label] = average_precision, num_annotations
+
+    return average_precisions
