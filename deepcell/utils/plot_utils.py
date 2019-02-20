5794978008e15ee1fb6552447e7e707b5ee42712
William Graf <7930703+willgraf@users.noreply.github.com>
2019-02-20 12:06:19 -0800

    add keras-retinanet visualization utils to plot_utils

--- deepcell/utils/plot_utils.py
+++ deepcell/utils/plot_utils.py
@@ -31,6 +31,7 @@ from __future__ import division
 
 import os
 
+import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import animation
@@ -49,6 +50,86 @@ def get_js_video(images, batch=0, channel=0, cmap='jet'):
     ani = animation.ArtistAnimation(fig, ims, interval=150, repeat_delay=1000)
     plt.close()
     return ani.to_jshtml()
+
+
+def draw_box(image, box, color, thickness=2):
+    """Draws a box on an image with a given color.
+    Args:
+        image: The image to draw on.
+        box: A list of 4 elements (x1, y1, x2, y2).
+        color: The color of the box.
+        thickness: The thickness of the lines to draw a box with.
+    """
+    b = np.array(box).astype(int)
+    cv2.rectangle(image, (b[0], b[1]), (b[2], b[3]), color, thickness, cv2.LINE_AA)
+
+
+def draw_caption(image, box, caption):
+    """Draws a caption above the box in an image.
+    Args:
+        image   : The image to draw on.
+        box     : A list of 4 elements (x1, y1, x2, y2).
+        caption : String containing the text to draw.
+    """
+    b = np.array(box).astype(int)
+    cv2.putText(image, caption, (b[0], b[1] - 10),
+                cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 0), 2)
+    cv2.putText(image, caption, (b[0], b[1] - 10),
+                cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 255, 255), 1)
+
+
+def draw_detections(image,
+                    boxes,
+                    scores,
+                    labels,
+                    color=[31, 0, 255],
+                    label_to_name=None,
+                    score_threshold=0.5):
+    """Draws detections in an image.
+    Args:
+        image: The image to draw on.
+        boxes: A [N, 4] matrix (x1, y1, x2, y2).
+        scores: A list of N classification scores.
+        labels: A list of N labels.
+        color: The color of the boxes.
+        label_to_name: (optional) Functor for mapping a label to a name.
+        score_threshold: Threshold used for determining what detections to draw.
+    """
+    selection = np.where(scores > score_threshold)[0]
+
+    for i in selection:
+        draw_box(image, boxes[i, :], color=color)
+
+        # draw labels
+        name = label_to_name(labels[i]) if label_to_name else labels[i]
+        caption = '{}: {0:.2f}'.format(name, scores[i])
+        draw_caption(image, boxes[i, :], caption)
+
+
+def draw_annotations(image,
+                     annotations,
+                     color=[31, 0, 255],
+                     label_to_name=None):
+    """Draws annotations in an image.
+    Args:
+        image: The image to draw on.
+        annotations: A [N, 5] matrix (x1, y1, x2, y2, label) or dictionary
+            containing bboxes (shaped [N, 4]) and labels (shaped [N]).
+        color: The color of the boxes.
+        label_to_name: (optional) Functor for mapping a label to a name.
+    """
+    if isinstance(annotations, np.ndarray):
+        annotations = {'bboxes': annotations[:, :4], 'labels': annotations[:, 4]}
+
+    assert 'bboxes' in annotations
+    assert 'labels' in annotations
+    assert annotations['bboxes'].shape[0] == annotations['labels'].shape[0]
+
+    for i in range(annotations['bboxes'].shape[0]):
+        label = annotations['labels'][i]
+        caption = '{}'.format(label_to_name(label) if label_to_name else label)
+        draw_caption(image, annotations['bboxes'][i], caption)
+        draw_box(image, annotations['bboxes'][i], color=color)
 
 
 def cf(x_coord, y_coord, sample_image):
