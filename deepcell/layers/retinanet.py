8614c2d5923cecd02f98b16ee3b602785a8b1aba
William Graf <7930703+willgraf@users.noreply.github.com>
2019-02-25 13:25:48 -0800

    add miscellaneous mrcnn layers

--- deepcell/layers/retinanet.py
+++ deepcell/layers/retinanet.py
@@ -227,3 +227,130 @@ class ClipBoxes(Layer):
         config = {'data_format': self.data_format}
         base_config = super(ClipBoxes, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
+
+
+class ConcatenateBoxes(Layer):
+    def call(self, inputs, **kwargs):
+        boxes, other = inputs
+        boxes_shape = K.shape(boxes)
+        other_shape = K.shape(other)
+        other = K.reshape(other, (boxes_shape[0], boxes_shape[1], -1))
+        return K.concatenate([boxes, other], axis=2)
+
+    def compute_output_shape(self, input_shape):
+        boxes_shape, other_shape = input_shape
+        return boxes_shape[:2] + (np.prod([s for s in other_shape[2:]]) + 4,)
+
+
+class RoiAlign(Layer):
+    def __init__(self, crop_size=(14, 14), **kwargs):
+        self.crop_size = crop_size
+        super(RoiAlign, self).__init__(**kwargs)
+
+    def map_to_level(self, boxes,
+                     canonical_size=224,
+                     canonical_level=1,
+                     min_level=0,
+                     max_level=4):
+        x1 = boxes[:, 0]
+        y1 = boxes[:, 1]
+        x2 = boxes[:, 2]
+        y2 = boxes[:, 3]
+
+        w = x2 - x1
+        h = y2 - y1
+
+        size = K.sqrt(w * h)
+
+        log = K.log(size / canonical_size + K.epsilon())
+        log2 = log / K.log(K.cast(2, log.dtype))
+
+        levels = tf.floor(canonical_level + log2)
+        levels = K.clip(levels, min_level, max_level)
+
+        return levels
+
+    def call(self, inputs, **kwargs):
+        # TODO: Support batch_size > 1
+        image_shape = K.cast(inputs[0], K.floatx())
+        boxes = K.stop_gradient(inputs[1][0])
+        scores = K.stop_gradient(inputs[2][0])
+        fpn = [K.stop_gradient(i[0]) for i in inputs[3:]]
+
+        # compute from which level to get features from
+        target_levels = self.map_to_level(boxes)
+
+        # process each pyramid independently
+        rois, ordered_indices = [], []
+        for i in range(len(fpn)):
+            # select the boxes and classification from this pyramid level
+            indices = tf.where(K.equal(target_levels, i))
+            ordered_indices.append(indices)
+
+            level_boxes = tf.gather_nd(boxes, indices)
+            fpn_shape = K.cast(K.shape(fpn[i]), dtype=K.floatx())
+
+            # convert to expected format for crop_and_resize
+            x1 = level_boxes[:, 0]
+            y1 = level_boxes[:, 1]
+            x2 = level_boxes[:, 2]
+            y2 = level_boxes[:, 3]
+            level_boxes = K.stack([
+                (y1 / image_shape[1] * fpn_shape[0]) / (fpn_shape[0] - 1),
+                (x1 / image_shape[2] * fpn_shape[1]) / (fpn_shape[1] - 1),
+                (y2 / image_shape[1] * fpn_shape[0] - 1) / (fpn_shape[0] - 1),
+                (x2 / image_shape[2] * fpn_shape[1] - 1) / (fpn_shape[1] - 1),
+            ], axis=1)
+
+            # append the rois to the list of rois
+            rois.append(tf.image.crop_and_resize(
+                K.expand_dims(fpn[i], axis=0),
+                level_boxes,
+                K.zeros((K.shape(level_boxes)[0],), dtype='int32'),
+                self.crop_size
+            ))
+
+        # concatenate rois to one blob
+        rois = K.concatenate(rois, axis=0)
+
+        # reorder rois back to original order
+        indices = K.concatenate(ordered_indices, axis=0)
+        rois = tf.scatter_nd(indices, rois, K.cast(K.shape(rois), 'int64'))
+
+        return K.expand_dims(rois, axis=0)
+
+    def compute_output_shape(self, input_shape):
+        output_shape = [
+            input_shape[1][0],
+            None,
+            self.crop_size[0],
+            self.crop_size[1],
+            input_shape[3][-1]
+        ]
+        return tensor_shape.TensorShape(output_shape)
+
+    def get_config(self):
+        config = {'crop_size': self.crop_size}
+        base_config = super(RoiAlign, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+
+class Shape(Layer):
+    def call(self, inputs, **kwargs):
+        return K.shape(inputs)
+
+    def compute_output_shape(self, input_shape):
+        return (len(input_shape),)
+
+
+class Cast(Layer):
+    def __init__(self, dtype=None, *args, **kwargs):
+        if dtype is None:
+            dtype = K.floatx()
+        self.dtype = dtype
+
+        super(Cast, self).__init__(*args, **kwargs)
+
+    def call(self, inputs, **kwargs):
+        outputs = K.cast(inputs, self.dtype)
+        return outputs
