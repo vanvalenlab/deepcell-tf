7f7d076986204b66dca81cfa0e77ab3d288db66a
William Graf <7930703+willgraf@users.noreply.github.com>
2019-08-13 19:49:09 -0700

    bring  back the mrcnn tests

--- deepcell/model_zoo/maskrcnn_test.py
+++ deepcell/model_zoo/maskrcnn_test.py
@@ -41,110 +41,110 @@ from tensorflow.python.platform import test
 from deepcell.model_zoo import MaskRCNN
 
 
-# class MaskRCNNTest(test.TestCase, parameterized.TestCase):
-#
-#     @parameterized.named_parameters([
-#         {
-#             'testcase_name': 'maskrcnn_basic',
-#             'pooling': None,
-#             'panoptic': False,
-#             'location': False,
-#             'nms': True,
-#             'class_specific_filter': True,
-#             'pyramid_levels': ['P3'],
-#         },
-#         {
-#             'testcase_name': 'maskrcnn_avgnorm',
-#             'pooling': 'avg',
-#             'panoptic': False,
-#             'location': False,
-#             'nms': False,
-#             'class_specific_filter': False,
-#             'pyramid_levels': ['P3', 'P4', 'P5'],
-#         },
-#         {
-#             'testcase_name': 'maskrcnn_panoptic_maxnorm',
-#             'pooling': 'max',
-#             'panoptic': True,
-#             'location': False,
-#             'nms': True,
-#             'class_specific_filter': True,
-#             'pyramid_levels': ['P5', 'P6', 'P7'],
-#         },
-#         {
-#             'testcase_name': 'maskrcnn_location',
-#             'pooling': 'max',
-#             'panoptic': True,
-#             'location': True,
-#             'nms': False,
-#             'class_specific_filter': True,
-#             'pyramid_levels': ['P3', 'P7'],
-#         },
-#         {
-#             'testcase_name': 'maskrcnn_panoptic_location',
-#             'pooling': 'max',
-#             'panoptic': True,
-#             'location': True,
-#             'nms': True,
-#             'class_specific_filter': False,
-#             'pyramid_levels': ['P3', 'P4', 'P5', 'P6', 'P7'],
-#         }
-#     ])
-#     @tf_test_util.run_in_graph_and_eager_modes()
-#     def test_maskrcnn(self, pooling, panoptic, location,
-#                       pyramid_levels, nms, class_specific_filter):
-#         num_classes = 3
-#         crop_size = (14, 14)
-#         mask_size = (28, 28)
-#
-#         max_detections = 10
-#         norm_method = None
-#         backbone = 'mobilenet'
-#
-#         # TODO: MaskRCNN fails with channels_first
-#         for data_format in ('channels_last',):  # 'channels_first'):
-#             with self.test_session(use_gpu=True):
-#                 K.set_image_data_format(data_format)
-#                 if data_format == 'channels_first':
-#                     axis = 1
-#                     input_shape = (1, 32, 32)
-#                 else:
-#                     axis = -1
-#                     input_shape = (32, 32, 1)
-#
-#                 num_semantic_classes = [3, 4]
-#                 model = MaskRCNN(
-#                     backbone=backbone,
-#                     num_classes=num_classes,
-#                     input_shape=input_shape,
-#                     norm_method=norm_method,
-#                     location=location,
-#                     pooling=pooling,
-#                     nms=nms,
-#                     class_specific_filter=class_specific_filter,
-#                     panoptic=panoptic,
-#                     crop_size=crop_size,
-#                     mask_size=mask_size,
-#                     max_detections=max_detections,
-#                     num_semantic_heads=len(num_semantic_classes),
-#                     num_semantic_classes=num_semantic_classes,
-#                     backbone_levels=['C3', 'C4', 'C5'],
-#                     pyramid_levels=pyramid_levels,
-#                 )
-#
-#                 expected_size = 7 + panoptic * len(num_semantic_classes)
-#                 self.assertIsInstance(model.output_shape, list)
-#                 self.assertEqual(len(model.output_shape), expected_size)
-#
-#                 self.assertEqual(model.output_shape[0][-1], 4)
-#                 self.assertEqual(model.output_shape[1][-1], num_classes)
-#
-#                 self.assertEqual(model.output_shape[3][-1], 4)
-#                 self.assertEqual(model.output_shape[4][-1], max_detections)
-#                 self.assertEqual(model.output_shape[5][-1], max_detections)
-#
-#                 self.assertEqual(model.output_shape[6][axis], num_classes)
-#
-#                 if panoptic:
-#                     for i, n in enumerate(num_semantic_classes):
-#                         self.assertEqual(model.output_shape[i + 7][axis], n)
+class MaskRCNNTest(test.TestCase, parameterized.TestCase):
+
+    @parameterized.named_parameters([
+        {
+            'testcase_name': 'maskrcnn_basic',
+            'pooling': None,
+            'panoptic': False,
+            'location': False,
+            'nms': True,
+            'class_specific_filter': True,
+            'pyramid_levels': ['P3'],
+        },
+        {
+            'testcase_name': 'maskrcnn_avgnorm',
+            'pooling': 'avg',
+            'panoptic': False,
+            'location': False,
+            'nms': False,
+            'class_specific_filter': False,
+            'pyramid_levels': ['P3', 'P4', 'P5'],
+        },
+        {
+            'testcase_name': 'maskrcnn_panoptic_maxnorm',
+            'pooling': 'max',
+            'panoptic': True,
+            'location': False,
+            'nms': True,
+            'class_specific_filter': True,
+            'pyramid_levels': ['P5', 'P6', 'P7'],
+        },
+        {
+            'testcase_name': 'maskrcnn_location',
+            'pooling': 'max',
+            'panoptic': True,
+            'location': True,
+            'nms': False,
+            'class_specific_filter': True,
+            'pyramid_levels': ['P3', 'P7'],
+        },
+        {
+            'testcase_name': 'maskrcnn_panoptic_location',
+            'pooling': 'max',
+            'panoptic': True,
+            'location': True,
+            'nms': True,
+            'class_specific_filter': False,
+            'pyramid_levels': ['P3', 'P4', 'P5', 'P6', 'P7'],
+        }
+    ])
+    # @tf_test_util.run_in_graph_and_eager_modes()
+    def test_maskrcnn(self, pooling, panoptic, location,
+                      pyramid_levels, nms, class_specific_filter):
+        num_classes = 3
+        crop_size = (14, 14)
+        mask_size = (28, 28)
+
+        max_detections = 10
+        norm_method = None
+        backbone = 'featurenet'  # not all backbones work with channels_first
+
+        # TODO: MaskRCNN fails with channels_first
+        for data_format in ('channels_last',):  # 'channels_first'):
+            with self.test_session(use_gpu=True):
+                K.set_image_data_format(data_format)
+                if data_format == 'channels_first':
+                    axis = 1
+                    input_shape = (1, 32, 32)
+                else:
+                    axis = -1
+                    input_shape = (32, 32, 1)
+
+                num_semantic_classes = [3, 4]
+                model = MaskRCNN(
+                    backbone=backbone,
+                    num_classes=num_classes,
+                    input_shape=input_shape,
+                    norm_method=norm_method,
+                    location=location,
+                    pooling=pooling,
+                    nms=nms,
+                    class_specific_filter=class_specific_filter,
+                    panoptic=panoptic,
+                    crop_size=crop_size,
+                    mask_size=mask_size,
+                    max_detections=max_detections,
+                    num_semantic_heads=len(num_semantic_classes),
+                    num_semantic_classes=num_semantic_classes,
+                    backbone_levels=['C3', 'C4', 'C5'],
+                    pyramid_levels=pyramid_levels,
+                )
+
+                expected_size = 7 + panoptic * len(num_semantic_classes)
+                self.assertIsInstance(model.output_shape, list)
+                self.assertEqual(len(model.output_shape), expected_size)
+
+                self.assertEqual(model.output_shape[0][-1], 4)
+                self.assertEqual(model.output_shape[1][-1], num_classes)
+
+                self.assertEqual(model.output_shape[3][-1], 4)
+                self.assertEqual(model.output_shape[4][-1], max_detections)
+                self.assertEqual(model.output_shape[5][-1], max_detections)
+
+                self.assertEqual(model.output_shape[6][axis], num_classes)
+
+                if panoptic:
+                    for i, n in enumerate(num_semantic_classes):
+                        self.assertEqual(model.output_shape[i + 7][axis], n)
