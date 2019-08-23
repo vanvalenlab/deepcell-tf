7934b373dd94b92ebe5263ca6813e0e36fe045a3
William Graf <7930703+willgraf@users.noreply.github.com>
2019-08-22 20:25:41 -0700

    ignore a bunch of 3rd party classes.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,6 +50,10 @@ extensions = [
     'm2r',
 ]
 
+napoleon_google_docstring = True
+
+default_role = 'py:obj'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -75,7 +79,7 @@ language = None
 exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = None
+pygments_style = 'sphinx'
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -168,6 +172,9 @@ texinfo_documents = [
 
 # Bibliographic Dublin Core info.
 epub_title = project
+epub_author = author
+epub_publisher = author
+epub_copyright = copyright
 
 # The unique identifier of the text. This can be a ISBN number
 # or the project homepage.
@@ -199,8 +206,24 @@ autodoc_mock_imports = [
 
 intersphinx_mapping = {
     'kiosk': ('https://deepcell-kiosk.readthedocs.io/en/latest/', None),
+    'python': ('https://docs.python.org/3.7', None),
+    'numpy': ('http://docs.scipy.org/doc/numpy/', None),
 }
 
 intersphinx_cache_limit = 0
 
 # -- Custom Additions --------------------------------------------------------
+nitpick_ignore = [
+    ('py:class', 'function'),  # TODO: set type for "function" properly
+    ('py:class', 'tensor'),  # TODO: set type for "tensor" properly
+    ('py:class', 'numpy.array'),
+    ('py:class', 'pandas.DataFrame'),
+    ('py:class', 'tensorflow.keras.Model'),
+    ('py:class', 'tensorflow.python.keras.Model'),
+    ('py:class', 'tensorflow.keras.layers.Layer'),
+    ('py:class', 'tensorflow.python.keras.layers.Layer'),
+    ('py:class', 'tensorflow.python.keras.layers.ZeroPadding2D'),
+    ('py:class', 'tensorflow.python.keras.layers.ZeroPadding3D'),
+    ('py:class', 'tensorflow.python.keras.preprocessing.image.Iterator'),
+    ('py:class', 'tensorflow.python.keras.preprocessing.image.ImageDataGenerator'),
+]
