a44fea124c2dea2ba729a4604e539cb8ecea6cc6
William Graf <7930703+willgraf@users.noreply.github.com>
2019-09-16 20:12:08 -0700

    filtering the boxes causing an issue?

--- deepcell/layers/filter_detections.py
+++ deepcell/layers/filter_detections.py
@@ -77,13 +77,13 @@ def filter_detections(boxes,
         indices = tf.where(K.greater(scores, score_threshold))
 
         if nms:
-            filtered_boxes = tf.gather_nd(boxes, indices)
-            filtered_scores = K.gather(scores, indices)[:, 0]
+            # filtered_boxes = tf.gather_nd(boxes, indices)
+            # filtered_scores = K.gather(scores, indices)[:, 0]
 
             # perform NMS
             nms_indices = tf.image.non_max_suppression(
-                filtered_boxes,
-                filtered_scores,
+                boxes,
+                scores,
                 max_output_size=max_detections,
                 score_threshold=score_threshold,
                 iou_threshold=nms_threshold)
