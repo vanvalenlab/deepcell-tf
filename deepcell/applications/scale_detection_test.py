ff2c1adb412744a8636a579bba4b0e48c1a01cf0
William Graf <7930703+willgraf@users.noreply.github.com>
2019-10-22 10:51:40 -0700

    update label and scale detection tests to use the featurenet backbone for time saving

--- deepcell/applications/scale_detection_test.py
+++ deepcell/applications/scale_detection_test.py
@@ -41,8 +41,8 @@ class TestScaleDetectionModel(test.TestCase):
 
     def test_scale_detection_model(self):
 
-        valid_backbones = ['VGG16']
-        input_shape = (256, 256, 1)  # channels will be set to 3
+        valid_backbones = ['featurenet']
+        input_shape = (216, 216, 1)  # channels will be set to 3
 
         batch_shape = tuple([8] + list(input_shape))
 
@@ -58,8 +58,8 @@ class TestScaleDetectionModel(test.TestCase):
 
                 y = model.predict(X)
 
-                assert y.shape[0] == X.shape[0]
                 assert len(y.shape) == 2
+                assert y.shape[0] == X.shape[0]
 
             with self.test_session():
                 model = ScaleDetectionModel(
@@ -69,5 +69,5 @@ class TestScaleDetectionModel(test.TestCase):
 
                 y = model.predict(X)
 
-                assert y.shape[0] == X.shape[0]
                 assert len(y.shape) == 2
+                assert y.shape[0] == X.shape[0]
