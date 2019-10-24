ca6aca443547710c97f55f5d146bf5cf53eedaab
willgraf <7930703+willgraf@users.noreply.github.com>
2019-10-24 09:48:08 -0700

    Remove weights from ImageNormalization layers. (#250)
    * set the kernel to W directly, do not use weights at all.
    
    * skip constraint and bias tests as self.kernel is no longer a weight.

--- deepcell/layers/normalization_test.py
+++ deepcell/layers/normalization_test.py
@@ -68,8 +68,8 @@ class ImageNormalizationTest(test.TestCase):
                 kernel_constraint=k_constraint,
                 bias_constraint=b_constraint)
             layer(keras.backend.variable(np.ones((3, 5, 6, 4))))
-            self.assertEqual(layer.kernel.constraint, k_constraint)
-            self.assertEqual(layer.bias.constraint, b_constraint)
+            # self.assertEqual(layer.kernel.constraint, k_constraint)
+            # self.assertEqual(layer.bias.constraint, b_constraint)
             # test bad norm_method
             with self.assertRaises(ValueError):
                 layer = layers.ImageNormalization2D(norm_method='invalid')
@@ -82,7 +82,7 @@ class ImageNormalizationTest(test.TestCase):
                 layer = layers.ImageNormalization2D()
                 layer.build([3, 5, 6, None])
 
-    # @tf_test_util.run_in_graph_and_eager_modes()
+    @tf_test_util.run_in_graph_and_eager_modes()
     def test_normalize_3d(self):
         custom_objects = {'ImageNormalization3D': layers.ImageNormalization3D}
         norm_methods = [None, 'std', 'max', 'whole_image']
@@ -111,8 +111,8 @@ class ImageNormalizationTest(test.TestCase):
                 kernel_constraint=k_constraint,
                 bias_constraint=b_constraint)
             layer(keras.backend.variable(np.ones((3, 4, 11, 12, 10))))
-            self.assertEqual(layer.kernel.constraint, k_constraint)
-            self.assertEqual(layer.bias.constraint, b_constraint)
+            # self.assertEqual(layer.kernel.constraint, k_constraint)
+            # self.assertEqual(layer.bias.constraint, b_constraint)
             # test bad norm_method
             with self.assertRaises(ValueError):
                 layer = layers.ImageNormalization3D(norm_method='invalid')
