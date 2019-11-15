9daa751a65562915cad01cdd40c1efa71d508fe2
William Graf <7930703+willgraf@users.noreply.github.com>
2019-11-15 13:42:53 -0800

    remove author tag, hardly true now.

--- deepcell/losses_test.py
+++ deepcell/losses_test.py
@@ -23,9 +23,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Tests for custom loss functions
-@author: David Van Valen
-"""
+"""Tests for custom loss functions"""
 
 from __future__ import absolute_import
 from __future__ import division
