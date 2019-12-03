e45650c6b29199eb8974ff745f5ad5e96a81012b
David Van Valen <vanvalen@gmail.com>
2019-12-03 11:43:01 -0800

    removing deepcell_tracking call

--- deepcell/utils/tracking_utils.py
+++ deepcell/utils/tracking_utils.py
@@ -29,4 +29,4 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
 
-from deepcell_tracking.utils import *
+# from deepcell_tracking.utils import *
