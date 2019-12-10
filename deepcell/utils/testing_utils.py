1f924a9a46bc4583131796e99c86956efd84c223
William Graf <7930703+willgraf@users.noreply.github.com>
2019-12-09 16:26:25 -0800

    PEP8

--- deepcell/utils/testing_utils.py
+++ deepcell/utils/testing_utils.py
@@ -201,7 +201,7 @@ def should_run_eagerly():
     """Returns whether the models we are testing should be run eagerly."""
     if _thread_local_data.run_eagerly is None:
         raise ValueError('Cannot call `should_run_eagerly()` outside of a '
-                        '`run_eagerly_scope()` or `run_all_keras_modes` '
-                        'decorator.')
+                         '`run_eagerly_scope()` or `run_all_keras_modes` '
+                         'decorator.')
 
     return _thread_local_data.run_eagerly and context.executing_eagerly()
