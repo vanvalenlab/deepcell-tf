be15f45699499c8a24ecfe2bc0845c0756ec8801
Morgan Schwartz <msschwartz21@gmail.com>
2020-03-30 13:50:12 -0700

    Check for None first

--- deepcell/model_zoo/panopticnet.py
+++ deepcell/model_zoo/panopticnet.py
@@ -68,11 +68,12 @@ def __merge_temporal_features(feature, mode='conv', feature_size=256, frames_per
             If mode=None, the output is exactly the input.
     """
 
-    acceptable_modes = {'conv', 'lstm', 'gru', 'none'}
-    mode = str(mode).lower()
-    if mode not in acceptable_modes:
-        raise ValueError('Mode {} not supported. Please choose from {}.'.format(
-            mode, str(acceptable_modes)))
+    acceptable_modes = {'conv', 'lstm', 'gru', None}
+    if mode is not None:
+        mode = str(mode).lower()
+        if mode not in acceptable_modes:
+            raise ValueError('Mode {} not supported. Please choose from {}.'.format(
+                mode, str(acceptable_modes)))
 
     if mode == 'conv':
         x = Conv3D(feature_size,
@@ -306,7 +307,8 @@ def PanopticNet(backbone,
         create_semantic_head (function): Function to get to build a
             semantic head submodel.
         frames_per_batch (int): Defaults to 1.
-        temporal_mode: Mode of temporal convolution. Choose from {'conv','lstm','gru', None}. Defaults to None.
+        temporal_mode: Mode of temporal convolution. Choose from {'conv','lstm','gru', None}.
+            Defaults to None.
         num_semantic_heads (int): Defaults to 1.
         num_semantic_classes (list): Defaults to [3].
         norm_method (str): ImageNormalization mode to use. Defaults to 'whole_image'
@@ -338,11 +340,12 @@ def PanopticNet(backbone,
     channel_axis = 1 if K.image_data_format() == 'channels_first' else -1
 
     # Check input to __merge_temporal_features
-    acceptable_modes = {'conv', 'lstm', 'gru', 'none'}
-    temporal_mode = str(temporal_mode).lower()
-    if temporal_mode not in acceptable_modes:
-        raise ValueError('Mode {} not supported. Please choose from {}.'.format(
-            temporal_mode, str(acceptable_modes)))
+    acceptable_modes = {'conv', 'lstm', 'gru', None}
+    if temporal_mode is not None:
+        temporal_mode = str(temporal_mode).lower()
+        if temporal_mode not in acceptable_modes:
+            raise ValueError('Mode {} not supported. Please choose from {}.'.format(
+                temporal_mode, str(acceptable_modes)))
 
     if inputs is None:
         if frames_per_batch > 1:
