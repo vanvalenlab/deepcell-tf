f91db1c3bbefb4cf3c07c8482aa636822177451c
Morgan Schwartz <msschwartz21@gmail.com>
2020-03-31 17:31:35 -0700

    Remove old nuclear model

--- deepcell/applications/nuclear_segmentation.py
+++ deepcell/applications/nuclear_segmentation.py
@@ -23,7 +23,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Tests for PhaseSegmentationModel"""
+"""Nuclear segmentation application"""
 
 from __future__ import absolute_import
 from __future__ import division
@@ -39,61 +39,9 @@ from deepcell.applications import SegmentationApplication
 from deepcell.model_zoo import PanopticNet
 
 
-# WEIGHTS_PATH = ('https://deepcell-data.s3-us-west-1.amazonaws.com/'
-#                 'model-weights/resnet50_panoptic_nuclear_segmentation.h5')
-
 WEIGHTS_PATH = ('https://deepcell-data.s3-us-west-1.amazonaws.com/'
                 'model-weights/general_nuclear_train_batch_size_82800_resnet50_'
                 '8_epochs_c4b2167eb754923856bc84fb29074413.h5')
-
-
-# def NuclearSegmentationModel(input_shape=(None, None, 1),
-#                              backbone='resnet50',
-#                              use_pretrained_weights=True):
-#     """
-#     A RetinaMask model with a ResNet50 backbone
-#     for nuclear segmentation trained on DAPI data.
-
-#     Args:
-#         input_shape (tuple): a 3-length tuple of the input data shape.
-#         backbone (str): name of the backbone to use for the model.
-#         use_pretrained_weights (bool): whether to load pre-trained weights.
-#             Only supports the ResNet50 backbone.
-#     """
-#     backbone_levels = ['C1', 'C2', 'C3', 'C4', 'C5']
-#     pyramid_levels = ['P2', 'P3', 'P4']
-#     anchor_size_dict = {'P2': 8, 'P3': 16, 'P4': 32}
-
-#     # Set up the prediction model
-#     anchor_params = generate_anchor_params(pyramid_levels, anchor_size_dict)
-#     model = model_zoo.RetinaMask(
-#         backbone=backbone,
-#         use_imagenet=False,
-#         panoptic=True,
-#         num_semantic_heads=2,
-#         num_semantic_classes=[4, 4],
-#         input_shape=input_shape,
-#         num_classes=1,
-#         backbone_levels=backbone_levels,
-#         pyramid_levels=pyramid_levels,
-#         anchor_params=anchor_params,
-#         norm_method='whole_image')
-
-#     if use_pretrained_weights:
-#         if backbone == 'resnet50':
-#             # '/data/models/resnet50_panoptic_train-val_DVV_V4.h5'
-#             weights_path = get_file(
-#                 'resnet50_panoptic_nuclear_segmentation.h5',
-#                 WEIGHTS_PATH,
-#                 cache_subdir='models',
-#                 md5_hash='6e925c49cb05a1e3b0e2210220922445')
-
-#             model.load_weights(weights_path)
-#         else:
-#             raise ValueError('Backbone %s does not have a weights file.' %
-#                              backbone)
-
-#     return model
 
 
 class NuclearSegmentationApplication(SegmentationApplication):
