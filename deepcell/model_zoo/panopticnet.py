c5c68de717f4971e9c803260cf95452c5a3e3055
cdpavelchek <cpavelchek@wustl.edu>
2020-06-25 11:46:32 -0700

    fixed 3D models to be trainable and able to load weights

--- deepcell/model_zoo/panopticnet.py
+++ deepcell/model_zoo/panopticnet.py
@@ -76,26 +76,37 @@ def __merge_temporal_features(feature, mode='conv', feature_size=256,
             raise ValueError('Mode {} not supported. Please choose '
                              'from {}.'.format(mode, str(acceptable_modes)))
 
+    f_name = str(feature.name)[:2]
+
+    conv_name = 'conv_mtf_{}'.format(f_name)
+    bnorm_name = 'bnorm_mtf_{}'.format(f_name)
+    acti_name = 'acti_mtf_{}'.format(f_name)
+
+    print('conv name is: ', conv_name)
+
     if mode == 'conv':
         x = Conv3D(feature_size,
                    (frames_per_batch, 3, 3),
                    strides=(1, 1, 1),
                    padding='same',
+                   name=conv_name,
                    )(feature)
-        x = BatchNormalization(axis=-1)(x)
-        x = Activation('relu')(x)
+        x = BatchNormalization(axis=-1, name=bnorm_name)(x)
+        x = Activation('relu', name=acti_name)(x)
     elif mode == 'lstm':
         x = ConvLSTM2D(feature_size,
                        (3, 3),
                        padding='same',
                        activation='relu',
-                       return_sequences=True)(feature)
+                       return_sequences=True,
+                       name=conv_name)(feature)
     elif mode == 'gru':
         x = ConvGRU2D(feature_size,
                       (3, 3),
                       padding='same',
                       activation='relu',
-                      return_sequences=True)(feature)
+                      return_sequences=True,
+                      name=conv_name)(feature)
     else:
         x = feature
 
@@ -280,7 +291,7 @@ def PanopticNet(backbone,
     features = [pyramid_dict[key] for key in pyramid_levels]
 
     if frames_per_batch > 1:
-        temporal_features = [__merge_temporal_features(f, mode=temporal_mode)
+        temporal_features = [__merge_temporal_features(f, mode=temporal_mode, frames_per_batch=frames_per_batch)
                              for f in features]
         for f, k in zip(temporal_features, pyramid_levels):
             pyramid_dict[k] = f
