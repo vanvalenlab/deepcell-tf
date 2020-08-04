979ffd11c0b786ff61821489383c5c64daf4959a
cdpavelchek <cpavelchek@wustl.edu>
2020-08-03 23:07:13 -0500

    fixed import error

--- deepcell/image_generators/semantic.py
+++ deepcell/image_generators/semantic.py
@@ -982,60 +982,6 @@ class SemanticMovieGenerator(ImageDataGenerator):
         return x, y
 
 
-# Copyright 2016-2019 The Van Valen Lab at the California Institute of
-# Technology (Caltech), with support from the Paul Allen Family Foundation,
-# Google, & National Institutes of Health (NIH) under Grant U24CA224309-01.
-# All rights reserved.
-#
-# Licensed under a modified Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.github.com/vanvalenlab/deepcell-tf/LICENSE
-#
-# The Work provided may be used for non-commercial academic purposes only.
-# For any other use of the Work, including commercial use, please contact:
-# vanvalenlab@gmail.com
-#
-# Neither the name of Caltech nor the names of its contributors may be used
-# to endorse or promote products derived from this software without specific
-# prior written permission.
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-"""Semantic segmentation data generators."""
-
-from __future__ import absolute_import
-from __future__ import print_function
-from __future__ import division
-
-import os
-
-import numpy as np
-
-from skimage.transform import rescale, resize
-
-from tensorflow.python.keras import backend as K
-from tensorflow.python.keras.preprocessing.image import array_to_img
-from tensorflow.python.keras.preprocessing.image import Iterator
-from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.python.platform import tf_logging as logging
-
-try:
-    import scipy
-    # scipy.linalg cannot be accessed until explicitly imported
-    from scipy import linalg
-    # scipy.ndimage cannot be accessed until explicitly imported
-    from scipy import ndimage
-except ImportError:
-    scipy = None
-
-from deepcell.image_generators import _transform_masks
-
 class Semantic3DIterator(Iterator):
     """Iterator yielding data from Numpy arrays (X and y).
 
