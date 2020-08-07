e726553fb2949683732eb64b87e0ef18a6169657
cdpavelchek <cpavelchek@wustl.edu>
2020-08-07 11:02:07 -0500

    fixed imports

--- deepcell/utils/__init__.py
+++ deepcell/utils/__init__.py
@@ -46,7 +46,6 @@ from deepcell.utils import tracking_utils
 from deepcell.utils import train_utils
 from deepcell.utils import transform_utils
 from deepcell.utils import retinanet_anchor_utils
-from deepcell.utils import utils_3D
 
 # Globally-importable utils.
 from deepcell.utils.data_utils import get_data
