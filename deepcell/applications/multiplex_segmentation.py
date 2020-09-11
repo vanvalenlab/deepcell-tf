1522e0fb52d283d24c4448e515f2e12745be6856
ngreenwald <noahfgreenwald@gmail.com>
2020-09-10 17:05:12 -0700

    initial commit

--- deepcell/applications/multiplex_segmentation.py
+++ deepcell/applications/multiplex_segmentation.py
@@ -31,10 +31,12 @@ from __future__ import print_function
 
 import os
 
+import numpy as np
+
 from tensorflow.python.keras.utils.data_utils import get_file
 
-from deepcell_toolbox.deep_watershed import deep_watershed_subcellular, format_output_multiplex
-from deepcell_toolbox.processing import phase_preprocess
+from deepcell_toolbox.deep_watershed import deep_watershed_mibi
+from deepcell_toolbox.processing import histogram_normalization, percentile_threshold
 
 from deepcell.applications import Application
 from deepcell.model_zoo import PanopticNet
@@ -42,6 +44,122 @@ from deepcell.model_zoo import PanopticNet
 
 WEIGHTS_PATH = ('https://deepcell-data.s3-us-west-1.amazonaws.com/'
                 'model-weights/Multiplex_Segmentation_20200816_compartment.h5')
+
+# processing functions
+
+
+def multiplex_preprocess(image, **kwargs):
+    """Preprocess input data for multiplex model
+
+    Args:
+        image: array to be processed
+
+    Returns:
+        np.array: processed image array
+    """
+    output = np.copy(image)
+    threshold = kwargs.get('threshold', True)
+    if threshold:
+        percentile = kwargs.get('percentile', 99.9)
+        output = percentile_threshold(image=output, percentile=percentile)
+
+    normalize = kwargs.get('normalize', True)
+    if normalize:
+        kernel_size = kwargs.get('kernel_size', 128)
+        output = histogram_normalization(image=output, kernel_size=kernel_size)
+
+    return output
+
+
+def format_output_multiplex(output_list):
+    """Takes list of model outputs and formats into a dictionary for better readability
+
+    Args:
+        output_list: list of predictions from semantic heads
+
+    Returns:
+        formatted_dict: dictionary with predictions
+
+    Raises: ValueError if model output list is not len(8)
+    """
+
+    if len(output_list) != 8:
+        raise ValueError('output_list was length {}, expecting length 8'.format(len(output_list)))
+
+    formatted_dict = {
+        'whole-cell': {
+            'inner-distance': output_list[0],
+            'outer-distance': output_list[1],
+            'fgbg-fg': output_list[2][..., :1],
+            'pixelwise-interior': output_list[3][..., 1:2]
+        },
+        'nuclear': {
+            'inner-distance': output_list[4],
+            'outer-distance': output_list[5],
+            'fgbg-fg': output_list[6][..., :1],
+            'pixelwise-interior': output_list[7][..., 1:2]
+        }
+    }
+
+    return formatted_dict
+
+
+def multiplex_postprocess(model_output, compartment='whole-cell', whole_cell_kwargs=None,
+                          nuclear_kwargs=None):
+    """Postprocess model output to generate predictions for distinct cellular compartments
+
+    Args:
+        model_output (dict): Output from deep watershed model. A dict with a key corresponding to
+            each cellular compartment with a model prediction. Each key maps to a subsequent dict
+            with the following keys entries
+            - inner-distance: Prediction for the inner distance transform.
+            - outer-distance: Prediction for the outer distance transform
+            - fgbg-fg: prediction for the foreground/background transform
+            - pixelwise-interior: Prediction for the interior/border/background transform.
+        compartment: which cellular compartments to generate predictions for.
+            must be one of 'whole_cell', 'nuclear', 'both'
+        whole_cell_kwargs (dict): Optional list of post-processing kwargs for whole-cell prediction
+        nuclear_kwargs (dict): Optional list of post-processing kwargs for nuclear prediction
+
+    Returns:
+        numpy.array: Uniquely labeled mask for each compartment
+
+    Raises:
+        ValueError: for invalid compartment flag
+    """
+
+    valid_compartments = ['whole-cell', 'nuclear', 'both']
+
+    if whole_cell_kwargs is None:
+        whole_cell_kwargs = {}
+
+    if nuclear_kwargs is None:
+        nuclear_kwargs = {}
+
+    if compartment not in valid_compartments:
+        raise ValueError('Invalid compartment supplied: {}. '
+                         'Must be one of {}'.format(compartment, valid_compartments))
+
+    if compartment == 'whole-cell':
+        label_images = deep_watershed_mibi(model_output=model_output['whole-cell'],
+                                           **whole_cell_kwargs)
+    elif compartment == 'nuclear':
+        label_images = deep_watershed_mibi(model_output=model_output['nuclear'],
+                                           **nuclear_kwargs)
+    elif compartment == 'both':
+        label_images_cell = deep_watershed_mibi(model_output=model_output['whole-cell'],
+                                                **whole_cell_kwargs)
+
+        label_images_nucleus = deep_watershed_mibi(model_output=model_output['nuclear'],
+                                                   **nuclear_kwargs)
+
+        label_images = np.concatenate((label_images_cell, label_images_nucleus), axis=-1)
+
+    else:
+        raise ValueError('Invalid compartment supplied: {}. '
+                         'Must be one of {}'.format(compartment, valid_compartments))
+
+    return label_images
 
 
 class MultiplexSegmentation(Application):
@@ -138,11 +256,13 @@ class MultiplexSegmentation(Application):
         super(MultiplexSegmentation, self).__init__(model,
                                                     model_image_shape=model_image_shape,
                                                     model_mpp=0.5,
-                                                    preprocessing_fn=phase_preprocess,
-                                                    postprocessing_fn=deep_watershed_subcellular,
+                                                    preprocessing_fn=multiplex_preprocess,
+                                                    postprocessing_fn=multiplex_postprocess,
                                                     format_model_output_fn=format_output_multiplex,
                                                     dataset_metadata=self.dataset_metadata,
                                                     model_metadata=self.model_metadata)
+    cell_defaults = {'interior_smooth': 1}
+    nuc_defaults = {'interior_smooth': 1}
 
     def predict(self,
                 image,
