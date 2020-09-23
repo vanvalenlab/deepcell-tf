7555f68251ffda2cd5fb1641645da29d23ab3fdc
William Graf <7930703+willgraf@users.noreply.github.com>
2020-09-22 19:08:44 -0700

    fix axis during transform

--- deepcell/image_generators/semantic.py
+++ deepcell/image_generators/semantic.py
@@ -157,9 +157,9 @@ class SemanticIterator(Iterator):
                 y_transform = _transform_masks(y_current, transform,
                                                data_format=self.data_format,
                                                **transform_kwargs)
-                if y_transform.shape[self.channel_axis - 1] > 1:
+                if y_transform.shape[self.channel_axis] > 1:
                     y_transform = np.asarray(y_transform, dtype='int32')
-                elif y_transform.shape[self.channel_axis - 1] == 1:
+                elif y_transform.shape[self.channel_axis] == 1:
                     y_transform = np.asarray(y_transform, dtype=K.floatx())
                 y_semantic_list.append(y_transform)
 
