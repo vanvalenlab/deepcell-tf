5da00da9d38acd318336d572b5affecd98181e23
William Graf <7930703+willgraf@users.noreply.github.com>
2020-11-02 23:16:27 -0800

    store all package metadata in __version__ file.

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
+#!/usr/bin/env python
+
 import logging
+import os
+
+from codecs import open
 
 try:
     from setuptools import setup, find_packages
 except ImportError:
     from distutils.core import setup, find_packages
+
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 
 def _parse_requirements(file_path):
@@ -24,17 +32,38 @@ except Exception:
     logging.warning('Failed to load requirements file, using default ones.')
     install_reqs = []
 
+
+try:
+    tests_reqs = _parse_requirements('requirements-test.txt')
+except Exception:
+    logging.warning('Failed to load test requirements file, using default ones.')
+    tests_reqs = []
+
+
+about = {}
+with open(os.path.join(here, 'deepcell', '__version__.py'), 'r', 'utf-8') as f:
+    exec(f.read(), about)
+
+
+with open('README.md', 'r', 'utf-8') as f:
+    readme = f.read()
+
+
 setup(
-    name='DeepCell',
-    version='0.6.0',
+    name=about['__title__'],
+    version=about['__version__'],
+    description=about['__description__'],
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    url=about['__url__'],
+    license=about['__license__'],
     packages=find_packages(),
     install_requires=install_reqs,
+    tests_require=tests_reqs,
     extras_require={
         'tests': ['pytest',
                   'pytest-cov'],
-    },
-    license='LICENSE',
-    author='David Van Valen',
-    author_email='vanvalen@caltech.edu',
-    description='Deep learning for single cell image segmentation',
+    }
 )
