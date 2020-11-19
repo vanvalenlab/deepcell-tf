ad4bdd345d5a44e3af1c166be036876947de086e
William Graf <7930703+willgraf@users.noreply.github.com>
2020-11-19 11:08:29 -0800

    Add some basic classifiers

--- setup.py
+++ setup.py
@@ -69,4 +69,14 @@ setup(
     description='Deep learning for single cell image segmentation',
     long_description=README,
     long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
 )
