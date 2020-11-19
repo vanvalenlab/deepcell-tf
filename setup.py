24ddd2f2de79ed9259623af037577e61d1999528
William Graf <7930703+willgraf@users.noreply.github.com>
2020-11-19 11:01:35 -0800

    Use _parse_requirements to get the required packages.

--- setup.py
+++ setup.py
@@ -31,33 +31,32 @@ except ImportError:
     from distutils.core import setup, find_packages
 
 
+
+
+
+
 def _parse_requirements(file_path):
-    lineiter = (line.strip() for line in open(file_path))
-    reqs = []
-    for line in lineiter:
-        # workaround to ignore keras_maskrcnn requirement
-        # which is downloaded directly from github
-        if line.startswith('#') or line.startswith('git+'):
-            continue
-        reqs.append(line)
-    return reqs
+    try:
+        lineiter = (line.strip() for line in open(file_path))
+        reqs = []
+        for line in lineiter:
+            # workaround to ignore keras_maskrcnn requirement
+            # which is downloaded directly from github
+            if line.startswith('#') or line.startswith('git+'):
+                continue
+            reqs.append(line)
+        return reqs
+    except Exception:
+        logging.warning('Failed to load %s, using default ones.', file_path)
+        return []
 
-
-try:
-    install_reqs = _parse_requirements('requirements.txt')
-except Exception:
-    logging.warning('Failed to load requirements file, using default ones.')
-    install_reqs = []
 
 setup(
     name='DeepCell',
     version='0.7.0',
     packages=find_packages(),
-    install_requires=install_reqs,
-    extras_require={
-        'tests': ['pytest',
-                  'pytest-cov'],
-    },
+    install_requires=_parse_requirements('requirements.txt'),
+    tests_require=_parse_requirements('requirements-test.txt'),
     license='LICENSE',
     author='David Van Valen',
     author_email='vanvalen@caltech.edu',
