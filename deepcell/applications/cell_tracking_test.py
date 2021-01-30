148061aeae0bc7187e0d33001187e448ace2f973
MekWarrior <moen.erick@gmail.com>
2021-01-29 16:21:12 -0800

    Convert CellTracking to TensorFlow 2.X style Application. (#473)
    * Switch tracking application from TensorFlow 1.x weights approach to TensorFlow 2.x `SavedModel`.
    
    * Update model file hash and organize model files to match `deepcell` standard.
    
    Co-authored-by: willgraf <7930703+willgraf@users.noreply.github.com>

--- deepcell/applications/cell_tracking_test.py
+++ deepcell/applications/cell_tracking_test.py
@@ -33,6 +33,7 @@ from tensorflow.python.platform import test
 import numpy as np
 import skimage as sk
 
+from deepcell.model_zoo.featurenet import siamese_model
 from deepcell.applications import CellTracking
 
 
@@ -65,8 +66,17 @@ class TestCellTracking(test.TestCase):
 
     def test_cell_tracking_app(self):
         with self.cached_session():
+            input_shape = (32, 32, 1)
+            neighborhood_scale_size = 30
+            features = {'appearance', 'distance', 'neighborhood', 'regionprop'}
+
+            model = siamese_model(
+                input_shape=input_shape,
+                neighborhood_scale_size=neighborhood_scale_size,
+                features=features)
+
             # test instantiation
-            app = CellTracking(use_pretrained_weights=False)
+            app = CellTracking(model=model)
 
             # test output shape
             shape = app.model.output_shape
