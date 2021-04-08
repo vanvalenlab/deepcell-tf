e1ca1c30add588f9f8bb1df9bf270fe1f762390a
MekWarrior <moen.erick@gmail.com>
2021-04-08 15:26:01 -0700

    update setup.py to include new dependencies

--- setup.py
+++ setup.py
@@ -62,6 +62,8 @@ setup(
         'scikit-image>=0.14.5',
         'scikit-learn>=0.20.4',
         'tensorflow==2.4.1',
+        'tensorflow_addons==0.12.1,'
+        'spektral==1.0.4',
         'jupyter>=1.0.0,<2',
         'opencv-python-headless<5',
         'deepcell-tracking>=0.3.1,<0.4.0',
