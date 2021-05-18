715ff68744dbe60c91a207f574a70d88e081833b
William Graf <7930703+willgraf@users.noreply.github.com>
2021-05-17 23:25:53 -0700

    Add deepcell.data module to migrate to tf.data.Datasets.

---
+++ deepcell/data/augment.py
@@ -0,0 +1,145 @@
+# Copyright 2016-2021 The Van Valen Lab at the California Institute of
+# Technology (Caltech), with support from the Paul Allen Family Foundation,
+# Google, & National Institutes of Health (NIH) under Grant U24CA224309-01.
+# All rights reserved.
+#
+# Licensed under a modified Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.github.com/vanvalenlab/deepcell-tf/LICENSE
+#
+# The Work provided may be used for non-commercial academic purposes only.
+# For any other use of the Work, including commercial use, please contact:
+# vanvalenlab@gmail.com
+#
+# Neither the name of Caltech nor the names of its contributors may be used
+# to endorse or promote products derived from this software without specific
+# prior written permission.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Functions for augmenting a ``tf.data.Dataset``."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import math
+
+import tensorflow as tf
+from tensorflow.keras import backend as K
+from tensorflow.python.platform import tf_logging as logging
+
+import numpy as np
+import tensorflow_addons as tfa
+
+
+def remove_images_without_objects(X, y, min_objects=1):
+    invalid_batches = []
+    # Remove images with small numbers of cells
+    for b in range(X.shape[0]):
+        if len(np.unique(y[b])) - 1 < min_objects:
+            invalid_batches.append(b)
+
+    invalid_batches = np.array(invalid_batches, dtype='int')
+
+    if invalid_batches.size > 0:
+        logging.warning('Removing %s of %s images with fewer than %s '
+                        'objects.', invalid_batches.size, X.shape[0],
+                        min_objects)
+
+    X = np.delete(X, invalid_batches, axis=0)
+    y = np.delete(y, invalid_batches, axis=0)
+    return X, y
+
+
+def transform_matrix_offset_center(matrix, x, y):
+    o_x = tf.constant(float(x) / 2 + 0.5, shape=(1,))
+    o_y = tf.constant(float(y) / 2 + 0.5, shape=(1,))
+    one = tf.constant(1.0, shape=(1,))
+    zero = tf.constant(0.0, shape=(1,))
+    offset_row_0 = tf.stack([one, zero, o_x], axis=1)
+    offset_row_1 = tf.stack([zero, one, o_y], axis=1)
+    offset_row_2 = tf.stack([zero, zero, one], axis=1)
+    offset_matrix = tf.concat([offset_row_0, offset_row_1, offset_row_2], axis=0)
+
+    reset_row_0 = tf.stack([one, zero, o_x], axis=1)
+    reset_row_1 = tf.stack([zero, one, o_y], axis=1)
+    reset_row_2 = tf.stack([zero, zero, one], axis=1)
+    reset_matrix = tf.concat([reset_row_0, reset_row_1, reset_row_2], axis=0)
+    # offset_matrix = np.array([[1, 0, o_x], [0, 1, o_y], [0, 0, 1]], dtype='float32')
+    # reset_matrix = np.array([[1, 0, -o_x], [0, 1, -o_y], [0, 0, 1]], dtype='float32')
+    # offset_matrix = tf.convert_to_tensor(offset_matrix)
+    # reset_matrix = tf.convert_to_tensor(reset_matrix)
+    transform_matrix = K.dot(K.dot(offset_matrix, matrix), reset_matrix)
+    return transform_matrix
+
+
+def random_rotate(arr, rotation_range=0):
+    """Randomly rotate centroids and appearances.
+
+    Args:
+        arr (Tensor): Dictionary of feature data.
+        rotation_range (int): Maximum rotation range in degrees.
+    Returns:
+        Tensor: Rotated ``arr`` data.
+    """
+    # Calculate the random rotation in radians
+    rg = rotation_range * math.pi / 180
+    theta = tf.random.uniform(shape=[1], minval=-rg, maxval=rg)
+
+    # Infer interpolation based on input data
+    interpolation = 'NEAREST'
+    if arr.dtype.isinteger and bool(tf.shape(arr)[-1] > 1):
+        interpolation = 'BILINEAR'
+
+    # Transform appearances
+    old_shape = tf.shape(arr)
+    new_shape = [-1, old_shape[2], old_shape[3], old_shape[4]]
+    img = tf.reshape(arr, new_shape)
+    img = tfa.image.rotate(img, theta, interpolation=interpolation)
+    img = tf.reshape(img, old_shape)
+    return img
+
+
+def random_zoom(arr, zoom_range):
+    one = tf.constant(1.0, shape=(1,))
+    zero = tf.constant(0.0, shape=(1,))
+    zx = tf.random.uniform(shape=(1,), minval=zoom_range[0], maxval=zoom_range[1])
+    zy = tf.random.uniform(shape=(1,), minval=zoom_range[0], maxval=zoom_range[1])
+    z_row_0 = tf.stack([zx, zero, zero], axis=1)
+    z_row_1 = tf.stack([zero, zy, zero], axis=1)
+    z_row_2 = tf.stack([zero, zero, one], axis=1)
+    zoom_matrix = tf.concat([z_row_0, z_row_1, z_row_2], axis=0)
+    h, w = arr.shape[1], arr.shape[2]
+    transform_matrix = transform_matrix_offset_center(zoom_matrix, h, w)
+
+    # Infer interpolation based on input data
+    interpolation = 'NEAREST'
+    if arr.dtype.isinteger and bool(tf.shape(arr)[-1] > 1):
+        interpolation = 'BILINEAR'
+
+    return tfa.image.transform(arr, transform_matrix, interpolation=interpolation)
+
+
+def apply_random_transform(image, rotation_range=0, zoom_range=0,
+                           vertical_flip=0, horizontal_flip=0):
+    """Randomly transform the image input array."""
+    if horizontal_flip:
+        image = tf.image.random_flip_left_right(image)
+
+    if vertical_flip:
+        image = tf.image.random_flip_up_down(image)
+
+    if rotation_range:
+        image = random_rotate(image, rotation_range)
+
+    if zoom_range:
+        image = random_zoom(image, zoom_range=zoom_range)
+
+    return image
---
+++ deepcell/data/semantic.py
@@ -0,0 +1,148 @@
+# Copyright 2016-2021 The Van Valen Lab at the California Institute of
+# Technology (Caltech), with support from the Paul Allen Family Foundation,
+# Google, & National Institutes of Health (NIH) under Grant U24CA224309-01.
+# All rights reserved.
+#
+# Licensed under a modified Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.github.com/vanvalenlab/deepcell-tf/LICENSE
+#
+# The Work provided may be used for non-commercial academic purposes only.
+# For any other use of the Work, including commercial use, please contact:
+# vanvalenlab@gmail.com
+#
+# Neither the name of Caltech nor the names of its contributors may be used
+# to endorse or promote products derived from this software without specific
+# prior written permission.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Prepare datasets for semantic segmentation"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+
+from deepcell.data import split_dataset
+from deepcell.data.augment import apply_random_transform
+from deepcell.data.augment import remove_images_without_objects
+from deepcell.image_generators import _transform_masks
+
+
+def transform_labels(y, transforms, transforms_kwargs=None,
+                     data_format='channels_last'):
+    """Copied from ``deepcell.image_generators.sematnic``"""
+    if transforms_kwargs is None:
+        transforms_kwargs = {}
+
+    channel_axis = len(y.shape) - 1 if data_format == 'channels_last' else 1
+
+    y_semantic_list = []
+
+    # loop over channels axis of labels in case there are multiple label types
+    slc = slice([None] * len(y.shape))
+    for c in range(y.shape[channel_axis]):
+        slc[channel_axis] = slice(c, c + 1)
+
+        y_current = y[slc]
+
+        for transform in transforms:
+            transform_kwargs = transforms_kwargs.get(transform, dict())
+            y_transform = _transform_masks(y_current, transform,
+                                           data_format=data_format,
+                                           **transform_kwargs)
+            y_semantic_list.append(y_transform)
+
+    return y_semantic_list
+
+
+def randomly_transform_images(X, y, **kwargs):
+    if not isinstance(y, list):
+        y = [y]
+
+    transformed_y = []
+
+    transformed_X = apply_random_transform(X, **kwargs)
+
+    for lbl in y:
+        transformed_lbl = apply_random_transform(lbl, **kwargs)
+        transformed_y.append(transformed_lbl)
+    
+    if len(transformed_y) == 1:
+        transformed_y = transformed_y[0]
+
+    return transformed_X, transformed_y
+
+
+def prepare_data(X, y, batch_size=32, buffer_size=256,
+                 seed=None, min_objects=1, val_split=0.2,
+                 rotation_range=0, zoom_range=0,
+                 horizontal_flip=False, vertical_flip=False,
+                 transforms=(None,), transform_kwargs=None):
+    """Build and prepare the tracking dataset.
+
+    Args:
+        X (tensor): A numpy array of the input data.
+        y (tensor): A numpy array of labels of the same shape as ``X``.
+        batch_size (int): number of examples per batch.
+        buffer_size (int): number of samples to buffer.
+        seed (int): Random seed.
+        track_length (int): Number of frames per example.
+        rotation_range (int): Maximum degrees to rotate inputs.
+        zoom_range (int): Maximum range to zoom in or out.
+        horizontal_flip (bool): Enable random left-right flipping.
+        vertical_flip (bool): Enable random up-down flipping.
+        val_split (float): Fraciton of data to split into validation set.
+
+    Returns:
+        tf.data.Dataset: A ``tf.data.Dataset`` object ready for training.
+    """
+    if transform_kwargs is None:
+        transform_kwargs = dict()
+
+    try:
+        if isinstance(transforms, str):
+            transforms = [transforms]
+        transforms = list(transforms)
+    except TypeError as err:
+        raise TypeError('transforms should be a list, found {}'.format(
+            type(transforms).__name__)) from err
+
+    X, y = remove_images_without_objects(X, y, min_objects=min_objects)
+
+    # create the semantic transforms before creating the dataset
+    # they require numpy and cannot be done by the Dataset itself
+    y_transform = transform_labels(y, transforms, transform_kwargs)
+
+    dataset = tf.data.Dataset.from_tensor_slices((X, y_transform))
+
+    dataset = dataset.shuffle(buffer_size, seed=seed).repeat()
+
+    # split into train/val before doing any augmentation
+    train_data, val_data = split_dataset(dataset, val_split)
+
+    # randomly rotate, flip, & zoom
+    rotate = lambda X, y: randomly_transform_images(X, y,
+                                                    rotation_range=rotation_range,
+                                                    horizontal_flip=horizontal_flip,
+                                                    vertical_flip=vertical_flip,
+                                                    zoom_range=zoom_range)
+    train_data = train_data.map(rotate, num_parallel_calls=tf.data.AUTOTUNE)
+
+    # batch the data
+    train_data = train_data.batch(batch_size)
+    val_data = val_data.batch(batch_size)
+
+    # prefetch the data
+    train_data = train_data.prefetch(tf.data.AUTOTUNE)
+    val_data = val_data.prefetch(tf.data.AUTOTUNE)
+
+    return train_data, val_data
