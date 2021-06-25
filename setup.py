a68e6c127436cdb1ea651768776ac4aa547cebdc
William Graf <7930703+willgraf@users.noreply.github.com>
2021-06-25 11:52:08 -0700

    Update tracking to ~0.4.1.

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
         'spektral==1.0.4',
         'jupyter>=1.0.0,<2',
         'opencv-python-headless<5',
-        'deepcell-tracking>=0.4.0,<0.5.0',
+        'deepcell-tracking~=0.4.1',
         'deepcell-toolbox~=0.10.0'
     ],
     extras_require={
