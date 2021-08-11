356ed700a8c1770db0d9f29f4c73d80fccec1a29
William Graf <7930703+willgraf@users.noreply.github.com>
2021-08-11 12:59:53 -0700

    Bump TensorFlow to 2.5.1

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
         'scipy>=1.2.3,<2',
         'scikit-image>=0.14.5',
         'scikit-learn>=0.20.4',
-        'tensorflow~=2.5.0',
+        'tensorflow~=2.5.1',
         'jupyter>=1.0.0,<2',
         'opencv-python-headless<5',
         'deepcell-tracking>=0.3.1,<0.4.0',
