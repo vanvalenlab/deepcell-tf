e675a68ee824eb78ec24b3bf1f2be599e9d8da69
Noah F. Greenwald <noahfgreenwald@gmail.com>
2021-08-16 14:31:27 -0700

    Support individual parameter passing to Mesmer app (#545)
    * Update default kwargs with any passed by the user.
    
    Co-authored-by: willgraf <7930703+willgraf@users.noreply.github.com>

--- deepcell/applications/mesmer_test.py
+++ deepcell/applications/mesmer_test.py
@@ -33,6 +33,8 @@ import pytest
 
 import numpy as np
 
+from unittest.mock import Mock
+
 from tensorflow.python.platform import test
 
 from deepcell.model_zoo import PanopticNet
@@ -221,6 +223,38 @@ class TestMesmer(test.TestCase):
             y = app.predict(x, compartment='both')
             self.assertEqual(x.shape[:-1], y.shape[:-1])
             self.assertEqual(y.shape[-1], 2)
+
+            # test that kwargs are passed through successfully
+            app._predict_segmentation = Mock()
+
+            # get defaults
+            _ = app.predict(x, compartment='whole-cell')
+            args = app._predict_segmentation.call_args[1]
+            default_cell_kwargs = args['postprocess_kwargs']['whole_cell_kwargs']
+            default_nuc_kwargs = args['postprocess_kwargs']['nuclear_kwargs']
+
+            # change one of the args for each compartment
+            maxima_threshold_cell = default_cell_kwargs['maxima_threshold'] + 0.1
+            radius_nuc = default_nuc_kwargs['radius'] + 2
+
+            _ = app.predict(x, compartment='whole-cell',
+                            postprocess_kwargs_whole_cell={'maxima_threshold':
+                                                           maxima_threshold_cell},
+                            postprocess_kwargs_nuclear={'radius': radius_nuc})
+
+            args = app._predict_segmentation.call_args[1]
+            cell_kwargs = args['postprocess_kwargs']['whole_cell_kwargs']
+            assert cell_kwargs['maxima_threshold'] == maxima_threshold_cell
+
+            nuc_kwargs = args['postprocess_kwargs']['nuclear_kwargs']
+            assert nuc_kwargs['radius'] == radius_nuc
+
+            # check that rest are unchanged
+            cell_kwargs['maxima_threshold'] = default_cell_kwargs['maxima_threshold']
+            assert cell_kwargs == default_cell_kwargs
+
+            nuc_kwargs['radius'] = default_nuc_kwargs['radius']
+            assert nuc_kwargs == default_nuc_kwargs
 
             # test legacy version
             old_app = MultiplexSegmentation(model)
