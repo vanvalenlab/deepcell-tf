0dbd4aad753b4d997d905c981fc2b52b28f5b83d
David Van Valen <vanvalen@gmail.com>
2022-05-23 15:17:38 -0700

    Update deepcell/utils/tfrecord_utils.py
    Co-authored-by: Morgan Schwartz <msschwartz21@gmail.com>

--- deepcell/utils/tfrecord_utils.py
+++ deepcell/utils/tfrecord_utils.py
@@ -23,7 +23,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""deepcell-toolbox Data Utilities"""
+"""Functions for reading and writing segmentation datasets as TF Records"""
 
 from __future__ import absolute_import
 from __future__ import division
