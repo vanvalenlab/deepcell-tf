a25123491d68c81ee7475671e52c82ba54ddff91
David Van Valen <vanvalen@gmail.com>
2022-06-03 19:33:52 -0700

    Fix tracking model bug that pinned n_filters, encoder_dim and embedding_dim to 64 (#606)
    * Adding hooks for get_image_features in Track class
    
    * Fixed a bug that prevented encoder_dim, embedding_dim, and n_filters from having values other than 64
    
    * Remove hooks for get_image_features from Track class so that this pull request is just about the tracking model's bug fix

--- deepcell/data/tracking.py
+++ deepcell/data/tracking.py
@@ -53,7 +53,8 @@ from deepcell.data import split_dataset
 class Track(object):  # pylint: disable=useless-object-inheritance
 
     def __init__(self, path=None, tracked_data=None,
-                 appearance_dim=32, distance_threshold=64):
+                 appearance_dim=32, distance_threshold=64,
+                 **kwargs):
         if tracked_data:
             training_data = tracked_data
         elif path:
