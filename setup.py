d2db953330459ff063133e0a8394f90f7a1549cb
Ross Barnowski <rossbar@berkeley.edu>
2023-01-06 11:58:46 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -72,14 +72,10 @@ setup(
         'deepcell-toolbox~=0.12.0'
     ],
     extras_require={
-        'tests': [
-            'pytest<6',
-            'pytest-cov',
-            'pytest-pep8',
-        ],
+        'tests': ['pytest', 'pytest-cov'],
     },
     packages=find_packages(),
-    python_requires='>=3.7, <3.10',
+    python_requires='>=3.7, <3.11',
     classifiers=[
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
@@ -89,6 +85,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
