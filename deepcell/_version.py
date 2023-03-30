283d9a5cd55be16a251c9535f6e7c17c3fb57d9f
Ross Barnowski <rossbar@caltech.edu>
2023-03-30 16:02:55 -0700

    Update copyright notice to 2023. (#658)

--- deepcell/_version.py
+++ deepcell/_version.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2022 The Van Valen Lab at the California Institute of
+# Copyright 2016-2023 The Van Valen Lab at the California Institute of
 # Technology (Caltech), with support from the Paul Allen Family Foundation,
 # Google, & National Institutes of Health (NIH) under Grant U24CA224309-01.
 # All rights reserved.
@@ -32,5 +32,5 @@ __download_url__ = '{}/tarball/{}'.format(__url__, __version__)
 __author__ = 'The Van Valen Lab'
 __author_email__ = 'vanvalen@caltech.edu'
 __license__ = 'LICENSE'
-__copyright__ = 'Copyright 2016-2022 The Van Valen Lab at the ' \
+__copyright__ = 'Copyright 2016-2023 The Van Valen Lab at the ' \
     'California Institute of Technology (Caltech)'
