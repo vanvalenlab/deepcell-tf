9001b9d2881e3e5be03c6b0182a3f5b73afacbf3
Morgan Schwartz <msschwartz21@gmail.com>
2023-07-12 16:13:02 -0700

    Bump version for 0.12.6 release (#674)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ copyright = ('2016-{currentyear}, Van Valen Lab at the '
 author = 'Van Valen Lab at Caltech'
 
 # The short X.Y.Z version - overriden by rtd
-version = release = '0.12.5'
+version = release = '0.12.6'
 
 import subprocess
 try:
