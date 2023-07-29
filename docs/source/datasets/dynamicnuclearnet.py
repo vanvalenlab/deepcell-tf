3e0608f043efe694f647c9582f5c518aaa1fe710
msschwartz21 <msschwartz21@gmail.com>
2023-07-28 17:29:29 -0700

    Successful thumbnails in gallery

--- docs/source/datasets/dynamicnuclearnet.py
+++ docs/source/datasets/dynamicnuclearnet.py
@@ -2,12 +2,15 @@
 DynamicNuclearNet
 =================
 
+.. image:: ../../images/tracked.gif
+    :width: 200pt
+    :align: center
+
 DynamicNuclearNet is a training dataset for nuclear segmentation and tracking published in
 Schwartz et al. 2023. The dataset is made up of two subsets, one for tracking and one for
 segmentation.
 
-This dataset is licensed under a modified Apache license for non-commercial academic use only
-http://www.github.com/vanvalenlab/deepcell-tf/LICENSE
+This dataset is licensed under a `modified Apache license <http://www.github.com/vanvalenlab/deepcell-tf/blob/master/LICENSE>`_ for non-commercial academic use only
 
 The dataset can be accessed using `deepcell.datasets` with a DeepCell API key.
 
@@ -15,14 +18,17 @@ TODO add api key info
 
 Tracking
 ^^^^^^^^
+
 Each batch of the dataset contains three components
+
 * X: raw fluorescent nuclear data
 * y: nuclear segmentation masks
-* lineages: lineage records including the cell id, frames present and division
-  links from parent to daughter cells
+* lineages: lineage records including the cell id, frames present and division links from parent to daughter cells
+
 """
 
 from deepcell.datasets.dynamic_nuclear_net import DynamicNuclearNetTracking
+# sphinx_gallery_thumbnail_path = '../images/tracked.gif'
 
 dnn_trk = DynamicNuclearNetTracking(version='1.0')
 X_val, y_val, lineage_val = dnn_trk.load_data(split='val')
@@ -32,6 +38,7 @@ data_source = dnn_trk.load_source_metadata()
 # Segmentation
 # ^^^^^^^^^^^^
 # Each batch of the dataset includes three components
+#
 # * X: raw fluorescent nuclear data
 # * y: nuclear segmentation masks
 # * metadata: description of the source of each batch
@@ -40,5 +47,3 @@ from deepcell.datasets.dynamic_nuclear_net import DynamicNuclearNetSegmentation
 
 dnn_seg = DynamicNuclearNetSegmentation(version='1.0')
 X_val, y_val, meta_val = dnn_seg.load_data(split='val')
-
-# sphinx_gallery_thumbnail_path = '../../images/3t3_nuclear_outlines.png'
