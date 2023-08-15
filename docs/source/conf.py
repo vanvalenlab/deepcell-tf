1b4f9779965df3f707434ee8db2b57694c49a1a8
Ross Barnowski <rossbar@berkeley.edu>
2023-08-15 11:34:25 -0700

    Rm duplicate (ignored) exclude_patterns from sphinx conf.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -106,11 +106,6 @@ master_doc = 'index'
 # Usually you set "language" from the command line for these cases.
 language = "en"
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
