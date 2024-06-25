0789628ddf64915804a629beeeafdae7ffd2f0fc
Ross Barnowski <rossbar@berkeley.edu>
2024-06-25 02:17:13 -0700

    Suppress warnings about nbsphinx breaking cache.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -94,7 +94,7 @@ source_suffix = ['.rst', '.md']
 # source_suffix = '.rst'
 
 # Ignore warning:
-suppress_warnings = ['autosectionlabel.*']
+suppress_warnings = ['autosectionlabel.*', "config.cache"]
 
 # The master toctree document.
 master_doc = 'index'
